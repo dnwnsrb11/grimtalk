505427204a6d390e83e7b61226c2eba5f39985d6
hoonixox <hoonixox@naver.com>
2025-01-22 20:05:42 +0900

    chore: tailwind css 설치 및 세팅 - tailwind css : className으로 스타일 적용 css 프레임워크 - postCSS : CSS를 브라우저에 맞게 변환해주는 도구 - Autoprefixer : 브라우저별 접두사 자동 추가 도구

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 function App() {
-  return <>grimtalk 그림톡</>;
+  return (
+    <div className="bg-blue-500 text-center text-3xl font-bold text-white">grimtalk 그림톡</div>
+  );
 }
 
 export default App;
