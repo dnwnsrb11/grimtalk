b1ede5f4666fbdb6c99ebd61f487ab70a01c1ed3
WooJunGyu <dnwnsrb11@naver.com>
2025-01-23 15:03:49 +0900

    test: 테스트를 위해 assets에 이미지 추가

--- src/components/mainPages/home/LiveList.jsx
+++ src/components/mainPages/home/LiveList.jsx
@@ -10,12 +10,14 @@ export const LiveList = () => {
         </div>
         <div>
           {/* 텍스트 구역 */}
-          <h4 className="text-lg">이모티콘을 배오고 싶은 당신을 위한 재밌는 강의</h4>
+          <h4 className="text-lg leading-tight">이모티콘을 배오고 싶은 당신을 위한 재밌는 강의</h4>
           <div className="flex items-center gap-3">
             <h4 className="text-base font-bold">Name</h4>
-            <div>
+            <div className="flex gap-1">
               <div className="inline-block rounded-full border bg-bg-gray-color px-3 py-1">
-                {/* 반복되어 보여진다. */}
+                <p className="text-text-gray-color">일러스트</p>
+              </div>
+              <div className="inline-block rounded-full border bg-bg-gray-color px-3 py-1">
                 <p className="text-text-gray-color">일러스트</p>
               </div>
             </div>
