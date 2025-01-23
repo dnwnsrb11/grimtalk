f99c9e3c631a5a987d3346c18c5aeb1678ced5a4
임재열 <dlawoduf15@naver.com>
2025-01-23 16:31:19 +0900

    chore: Signup-page 경로 추가

---
+++ src/routes/pages/SignupPage.jsx
@@ -0,0 +1,36 @@
+export const SignupPage = () => {
+  return (
+    <div className="relative flex h-full items-center justify-center gap-2">
+      <div className="relative z-10 w-80 rounded-lg bg-white bg-opacity-80 p-5 shadow-lg">
+        <div className="mb-1 flex flex-row justify-between">
+          <p className="text-xl">로그인</p>
+          <p className="pt-2 text-xs">오신 것을 환영합니다.</p>
+        </div>
+
+        <div className="flex flex-col gap-3">
+          <input
+            type="text"
+            placeholder="이메일을 입력하세요"
+            className="h-10 rounded-md border border-gray-border-color pl-3"
+          />
+          <input
+            type="text"
+            placeholder="비밀번호를 입력하세요"
+            className="h-10 rounded-md border border-gray-border-color pl-3"
+          />
+          <div className="flex justify-center">
+            <button
+              type="submit"
+              className="h-10 w-full rounded-full bg-primary-color text-center text-white"
+            >
+              로그인
+            </button>
+          </div>
+          <div className="text-center text-xs">
+            <button type="submit">회원가입</button> | <button type="submit">비밀번호 찾기</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
