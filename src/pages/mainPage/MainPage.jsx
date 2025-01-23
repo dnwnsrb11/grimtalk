f93953caee437d0f595f9d9b34239934dbeea9a9
WooJunGyu <dnwnsrb11@naver.com>
2025-01-23 12:44:49 +0900

    feat create mainpage 1. 메인페이지 jsx 생성 2. 메인페이지에 들어가는 라이브 컴포넌트 추가

---
+++ src/pages/mainPage/MainPage.jsx
@@ -0,0 +1,33 @@
+export const MainPage = () => {
+  return (
+    <>
+      <div>
+        <h2>
+          인기 있는 <span>라이브</span>
+        </h2>
+        <div>{/* 라이브 컴포넌트 */}</div>
+      </div>
+      <div>{/* 그림 태그 컴포넌트 */}</div>
+      <div>
+        <div>
+          <h2>
+            <span>인기</span> 강사
+          </h2>
+          <div>{/* 인기강사 */}</div>
+        </div>
+        <div>
+          <h2>
+            요즘 <span>뜨는</span> 강사
+          </h2>
+          <div>{/* 인기강사 */}</div>
+        </div>
+      </div>
+      <div>
+        <h2>
+          <span>인기</span> 있는 강의
+        </h2>
+        <div>{/* 인기 있는 강사 컴포넌트 */}</div>
+      </div>
+    </>
+  );
+};
