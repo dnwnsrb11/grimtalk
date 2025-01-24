0e0a2ed36eda2312397ba2ffe88153221acd6c4e
WooJunGyu <dnwnsrb11@naver.com>
2025-01-24 16:06:12 +0900

    feat: 마우스 호버시 디자인 변화 기능 추가

--- src/components/mainPages/community/CommunityList.jsx
+++ src/components/mainPages/community/CommunityList.jsx
@@ -2,15 +2,25 @@ import { useState } from 'react';
 
 export const CommunityList = () => {
   const [checkA, setCheckA] = useState(true);
+  const [mouseCheck, setMouseCheck] = useState(false);
+
+  const mouseEnter = () => {
+    setMouseCheck(true);
+  };
+  const mouseLeave = () => {
+    setMouseCheck(false);
+  };
   return (
     <>
-      <div className="mb-[20px] flex cursor-pointer flex-col rounded-3xl border border-gray-border-color p-5">
+      <div
+        className={`mb-[20px] flex cursor-pointer flex-col rounded-3xl border px-[22px] py-[15px] transition-colors duration-300 ease-in-out  ${mouseCheck ? 'border-primary-color bg-[#ffd8cf]' : ' border-gray-border-color'}`}
+        onMouseEnter={mouseEnter}
+        onMouseLeave={mouseLeave}
+      >
         <div className="mb-3">
-          {/* 제목 */}
           <h2 className="text-[22px] font-bold">질문이 있습니다</h2>
         </div>
         <div className="flex justify-between">
-          {/* 처음 */}
           <div className="flex gap-2">
             <div className="rounded-full border bg-bg-gray-color p-1 px-5">
               <p>이모티콘으로 부자가 되는 법 12월 수업</p>
