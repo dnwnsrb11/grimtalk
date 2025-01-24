b87dea9b7ef5577b67363cc6d53b8928f4b1449f
WooJunGyu <dnwnsrb11@naver.com>
2025-01-24 15:27:08 +0900

    feat:커뮤니티 리스트 컴포넌트 생성

---
+++ src/components/mainPages/community/CommunityList.jsx
@@ -0,0 +1,14 @@
+export const CommunityList = () => {
+  return (
+    <>
+      <div>
+        <div>{/* 제목 */}</div>
+        <div>
+          {/* 인포 */}
+          <div>{/* 처음 */}</div>
+          <div>{/* 뒤에 */}</div>
+        </div>
+      </div>
+    </>
+  );
+};
