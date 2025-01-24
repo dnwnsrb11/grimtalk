a082764f9cf6d067fbd2643ebaca36a910eabf43
hoonixox <hoonixox@naver.com>
2025-01-24 16:05:53 +0900

    feat: 유저소개일 때, 수정하기 버튼이 나오도록 구현

--- src/layouts/MyPageContentLayout.jsx
+++ src/layouts/MyPageContentLayout.jsx
@@ -3,10 +3,23 @@ export const MyPageContentLayout = ({
   navMenuTitle, // 현재 선택된 네비게이션 메뉴 제목
   navMenuContent, // 선택된 메뉴에 해당하는 컨텐츠
 }) => {
+  // 유저 소개일 때 메뉴 타이틀에 출력될 컴포넌트
+  const memberInfoMenuTitleSection = (
+    <div className="flex flex-row gap-3">
+      <div className="text-2xl font-bold">{navMenuTitle}</div>
+      <button className="rounded-lg bg-bg-gray-color px-2 py-1 text-lg">수정하기</button>
+    </div>
+  );
+
   return (
     <>
       {/* 메뉴 제목 */}
-      <div className="text-2xl font-bold">{navMenuTitle}</div>
+      {navMenuTitle === '유저소개' ? (
+        memberInfoMenuTitleSection
+      ) : (
+        <div className="text-2xl font-bold">{navMenuTitle}</div>
+      )}
+
       {/* 구분선 */}
       <hr className="w-full" />
       {/* 메뉴 컨텐츠 */}
