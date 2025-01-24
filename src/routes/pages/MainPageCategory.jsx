0f33d00f04546deb9ffa51cb2c605b43aee37808
WooJunGyu <dnwnsrb11@naver.com>
2025-01-24 10:21:27 +0900

    feat: 카테고리에 사용할 아이콘 assets에 추가

--- src/routes/pages/MainPageCategory.jsx
+++ src/routes/pages/MainPageCategory.jsx
@@ -1,18 +1,16 @@
-export const MainPageCategory = () => {
-    return (
-        <>
-        <div>
-        {/* 배너 */}
-            <div>
+import { Banner } from '@/components/mainPages/home/Banner';
 
-            </div>
-            <div>
-                {/* 카테고리 */}
-            </div>
-            <div>
-                {/* 목록 */}
-            </div>
+export const MainPageCategory = () => {
+  return (
+    <>
+      <div>
+        <div>
+          {/* 배너 */}
+          <Banner />
         </div>
-        </>
-    )
-}
+        <div>{/* 카테고리 */}</div>
+        <div>{/* 목록 */}</div>
+      </div>
+    </>
+  );
+};
