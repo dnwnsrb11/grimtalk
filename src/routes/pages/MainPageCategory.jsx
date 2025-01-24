782dd3ea285fd74279fa5438f9f84e3ef8c49ded
WooJunGyu <dnwnsrb11@naver.com>
2025-01-24 10:12:46 +0900

    feat: 카테고리 페이지 div 구성

--- src/routes/pages/MainPageCategory.jsx
+++ src/routes/pages/MainPageCategory.jsx
@@ -1,5 +1,18 @@
 export const MainPageCategory = () => {
     return (
-        
+        <>
+        <div>
+        {/* 배너 */}
+            <div>
+
+            </div>
+            <div>
+                {/* 카테고리 */}
+            </div>
+            <div>
+                {/* 목록 */}
+            </div>
+        </div>
+        </>
     )
 }
