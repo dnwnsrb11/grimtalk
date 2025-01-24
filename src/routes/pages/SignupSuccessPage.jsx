a81a5c44b7a5caca8bc216873b9de8157e770c71
임재열 <dlawoduf15@naver.com>
2025-01-24 13:11:05 +0900

    design/피그마 토대로 회원가입 페이지 기본 구현

---
+++ src/routes/pages/SignupSuccessPage.jsx
@@ -0,0 +1,33 @@
+export const SignupSuccessPage = () => {
+  return (
+    <div className="mx-auto flex h-full flex-col items-center justify-center">
+      <div className="flex flex-col  justify-center">
+        {/* 문장 전체를 감싸는 flex 컨테이너 */}
+        <div className="flex flex-row">
+          <div className="flex flex-col">
+            <h1 className="mb-[20px]">
+              <span className="mr-2 text-5xl font-bold">OOO님</span>
+              <span className="text-5xl font-bold text-primary-color">가입을 환영해요.</span>
+            </h1>
+            <p className="text-3xl text-[#A8A8A8]">가입이 완료되었습니다.</p>
+            <p className="text-3xl text-[#A8A8A8]">그림톡에서 미술 실력을 키워보세요.</p>
+          </div>
+
+          {/* 이미지 */}
+          <img
+            src="/src/assets/Vector.png"
+            alt=""
+            className="h-30 ml-[51px] w-auto flex-shrink-0"
+          />
+        </div>
+        <div className="mt-[40px] flex">
+          <button className="text-s h-[53px] w-[304px] rounded-3xl bg-primary-color text-white">
+            로그인 창으로 →
+          </button>
+        </div>
+      </div>
+
+      {/* 버튼을 텍스트들 아래로 배치 */}
+    </div>
+  );
+};
