20056ec5c064db94cef5919e6cb6fc4e0d11795d
WooJunGyu <dnwnsrb11@naver.com>
2025-01-24 16:26:29 +0900

    feat:배너 배치

--- src/routes/pages/MainPageLive.jsx
+++ src/routes/pages/MainPageLive.jsx
@@ -1,5 +1,16 @@
+import { Banner } from '@/components/mainPages/home/Banner';
+
 export const MainPageLive = () => {
-    return (
-        
-    )
-}
+  return (
+    <>
+      <div>
+        <div>
+          <Banner />
+        </div>
+        <div>{/* 인기 라이브 */}</div>
+        <hr />
+        <div>{/* 전체 라이브 */}</div>
+      </div>
+    </>
+  );
+};
