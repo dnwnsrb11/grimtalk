455abe273507755b1f92f0152989d3b4822d3232
WooJunGyu <dnwnsrb11@naver.com>
2025-01-24 15:51:38 +0900

    desing: 질문 리스트 디자인 완료

--- src/components/mainPages/community/CommunityList.jsx
+++ src/components/mainPages/community/CommunityList.jsx
@@ -4,26 +4,26 @@ export const CommunityList = () => {
   const [checkA, setCheckA] = useState(true);
   return (
     <>
-      <div className="flex flex-col rounded-2xl border p-5">
-        <div>
+      <div className="flex flex-col rounded-3xl border border-gray-border-color p-5">
+        <div className="mb-3">
           {/* 제목 */}
-          <h2>질문이 있습니다</h2>
+          <h2 className="text-[22px] font-bold">질문이 있습니다</h2>
         </div>
         <div className="flex justify-between">
           {/* 처음 */}
-          <div className="flex gap-3">
-            <div>
+          <div className="flex gap-2">
+            <div className="rounded-full border bg-bg-gray-color p-1 px-5">
               <p>이모티콘으로 부자가 되는 법 12월 수업</p>
             </div>
-            <div>
+            <div className="rounded-full border bg-bg-gray-color p-1 px-5">
               <p>2025년 1월 20일</p>
             </div>
-            <div>
-              <p>답변완료</p>
+            <div className="rounded-full border bg-primary-color p-1 px-5">
+              <p className="text-white">답변완료</p>
             </div>
           </div>
           <div>
-            <div>
+            <div className="rounded-full border border-[#AEAEAE]  p-1 px-5">
               <p>크와와와왕</p>
             </div>
           </div>
