4ffd9d58890116493b0d2fe46a4a58f799582062
hoonixox <hoonixox@naver.com>
2025-01-24 17:19:36 +0900

    design: my page nav-bar 폰트 색상 변경

--- src/components/mypage/NavigationMenu.jsx
+++ src/components/mypage/NavigationMenu.jsx
@@ -4,13 +4,15 @@ export const NavigationMenu = ({ selectedMenu, onMenuSelect }) => {
   const menuItems = ['유저소개', '대시보드', '마이 페이지', '내가 쓴 글', '구독, 즐겨찾기'];
 
   return (
-    <div className="flex flex-col items-baseline gap-3 text-xl">
+    <div className="mt-[40px] flex flex-col items-baseline gap-3 text-xl">
       {/* 메뉴 아이템들을 매핑하여 버튼으로 렌더링 */}
       {menuItems.map((item) => (
         <button
           key={item}
           onClick={() => onMenuSelect(item)}
-          className={`hover:font-semibold ${selectedMenu === item ? 'font-bold' : ''}`}
+          className={`hover:font-semibold ${
+            selectedMenu === item ? 'font-bold text-black' : 'text-[#C6C6C6] hover:text-black'
+          }`}
         >
           {item}
         </button>
