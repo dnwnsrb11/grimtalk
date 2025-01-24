cf16adb8f176b2cf83a2e0de62f0303f3d971029
WooJunGyu <dnwnsrb11@naver.com>
2025-01-24 16:48:38 +0900

    feat:라이브 페이지에 컴포넌트 배치 완료

--- src/routes/pages/MainPageLive.jsx
+++ src/routes/pages/MainPageLive.jsx
@@ -1,4 +1,5 @@
 import { Banner } from '@/components/mainPages/home/Banner';
+import { LiveList } from '@/components/mainPages/home/LiveList';
 
 export const MainPageLive = () => {
   return (
@@ -7,9 +8,31 @@ export const MainPageLive = () => {
         <div>
           <Banner />
         </div>
-        <div>{/* 인기 라이브 */}</div>
-        <hr />
-        <div>{/* 전체 라이브 */}</div>
+        <div className="mt-[50px]">
+          <h2 className="mb-[15px] text-2xl font-bold">
+            인기 있는 <span className="text-primary-color">라이브</span>
+          </h2>
+          <div className="mb-[50px] flex gap-3">
+            {Array.from({ length: 4 }, (_, index) => (
+              <LiveList />
+            ))}
+          </div>
+        </div>
+        <hr className="border-[#ACACAC]" />
+        <div>
+          <div className="mt-[50px]">
+            <h2 className="mb-[15px] text-2xl font-bold">
+              전체 <span className="text-primary-color">라이브</span>
+            </h2>
+            <div className="mb-[50px] flex flex-wrap gap-3">
+              {Array.from({ length: 8 }, (_, index) => (
+                <div key={index} className="mb-[40px] w-[calc(25%_-_0.75rem)]">
+                  <LiveList />
+                </div>
+              ))}
+            </div>
+          </div>
+        </div>
       </div>
     </>
   );
