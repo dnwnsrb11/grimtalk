69d112a62dbdcfb6e42cc7ef19fdfd9b53fd5676
WooJunGyu <dnwnsrb11@naver.com>
2025-01-24 13:28:30 +0900

    desgin: 카테고리 전체 배치 완료 1. 클릭시 활성화 비활성화 기능도 동일하게 적용 완료

--- src/components/mainPages/home/category/CategoryList.jsx
+++ src/components/mainPages/home/category/CategoryList.jsx
@@ -28,7 +28,7 @@ export const CategoryList = () => {
         <div className="flex flex-col items-center">
           <div
             onClick={() => changeCategory('all')}
-            className={`rounded-xl border border-[#C6C6C6] p-5 ${categoryName === 'all' ? 'shadow-md' : ''}`}
+            className={`max-h-[70px] rounded-xl border border-[#C6C6C6] p-5 ${categoryName === 'all' ? 'shadow-md' : ''}`}
           >
             {categoryName === 'all' ? (
               <img src={allActiveSVG} alt="" />
@@ -41,6 +41,126 @@ export const CategoryList = () => {
               <h3 className="text-lg font-bold text-primary-color">전체</h3>
             ) : (
               <h3 className="text-lg font-medium ">전체</h3>
+            )}
+          </div>
+        </div>
+        {/* 캐릭터 */}
+        <div className="flex flex-col items-center">
+          <div
+            onClick={() => changeCategory('character')}
+            className={`flex  max-h-[70px] rounded-xl border border-[#C6C6C6] p-5 ${categoryName === 'character' ? 'shadow-md' : ''}`}
+          >
+            {categoryName === 'character' ? (
+              <img src={characterActiveSVG} alt="" />
+            ) : (
+              <img src={characterDeactiveSVG} alt="" />
+            )}
+          </div>
+          <div className="mt-1">
+            {categoryName === 'character' ? (
+              <h3 className="text-lg font-bold text-primary-color">캐릭터</h3>
+            ) : (
+              <h3 className="text-lg font-medium ">캐릭터</h3>
+            )}
+          </div>
+        </div>
+        {/* 이모티콘 */}
+        <div className="flex flex-col items-center">
+          <div
+            onClick={() => changeCategory('emoticon')}
+            className={`flex  max-h-[70px] rounded-xl border border-[#C6C6C6] p-5 ${categoryName === 'emoticon' ? 'shadow-md' : ''}`}
+          >
+            {categoryName === 'emoticon' ? (
+              <img src={emoticonActiveSVG} alt="" />
+            ) : (
+              <img src={emoticonDeactiveSVG} alt="" />
+            )}
+          </div>
+          <div className="mt-1">
+            {categoryName === 'emoticon' ? (
+              <h3 className="text-lg font-bold text-primary-color">이모티콘</h3>
+            ) : (
+              <h3 className="text-lg font-medium ">이모티콘</h3>
+            )}
+          </div>
+        </div>
+        {/* 드로잉 */}
+        <div className="flex flex-col items-center">
+          <div
+            onClick={() => changeCategory('drawing')}
+            className={`flex  max-h-[70px] rounded-xl border border-[#C6C6C6] p-5 ${categoryName === 'drawing' ? 'shadow-md' : ''}`}
+          >
+            {categoryName === 'drawing' ? (
+              <img src={drawingActiveSVG} alt="" />
+            ) : (
+              <img src={drawingDeactiveSVG} alt="" />
+            )}
+          </div>
+          <div className="mt-1">
+            {categoryName === 'drawing' ? (
+              <h3 className="text-lg font-bold text-primary-color">드로잉</h3>
+            ) : (
+              <h3 className="text-lg font-medium ">드로잉</h3>
+            )}
+          </div>
+        </div>
+        {/* 컬러링 */}
+        <div className="flex flex-col items-center">
+          <div
+            onClick={() => changeCategory('coloring')}
+            className={`flex  max-h-[70px] rounded-xl border border-[#C6C6C6] p-5 ${categoryName === 'coloring' ? 'shadow-md' : ''}`}
+          >
+            {categoryName === 'coloring' ? (
+              <img src={coloringActiveSVG} alt="" />
+            ) : (
+              <img src={coloringDeactiveSVG} alt="" />
+            )}
+          </div>
+          <div className="mt-1">
+            {categoryName === 'coloring' ? (
+              <h3 className="text-lg font-bold text-primary-color">컬러링</h3>
+            ) : (
+              <h3 className="text-lg font-medium ">컬러링</h3>
+            )}
+          </div>
+        </div>
+        {/* 웹툰 */}
+        <div className="flex flex-col items-center">
+          <div
+            onClick={() => changeCategory('webtoon')}
+            className={`flex  max-h-[70px] rounded-xl border border-[#C6C6C6] p-5 ${categoryName === 'webtoon' ? 'shadow-md' : ''}`}
+          >
+            {categoryName === 'webtoon' ? (
+              <img src={webtoonActiveSVG} alt="" />
+            ) : (
+              <img src={webtoonDeactiveSVG} alt="" />
+            )}
+          </div>
+          <div className="mt-1">
+            {categoryName === 'webtoon' ? (
+              <h3 className="text-lg font-bold text-primary-color">웹툰</h3>
+            ) : (
+              <h3 className="text-lg font-medium ">웹툰</h3>
+            )}
+          </div>
+        </div>
+        {/* 컨셉 아트 */}
+        <div className="flex flex-col items-center">
+          <div
+            onClick={() => changeCategory('art')}
+            className={`flex  max-h-[70px] rounded-xl border border-[#C6C6C6] p-5 ${categoryName === 'art' ? 'shadow-md' : ''}`}
+          >
+            {categoryName === 'art' ? (
+              <img src={artActiveSVG} alt="" />
+            ) : (
+              <img src={artDeactiveSVG} alt="" />
+            )}
+          </div>
+          <div className="mt-1">
+            {categoryName === 'art' ? (
+              <h3 className="text-lg font-bold text-primary-color">컨셉 아트</h3>
+            ) : (
+              <h3 className="text-lg font-medium ">컨셉 아트</h3>
             )}
           </div>
         </div>
