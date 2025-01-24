a47252fc14c056cd9d6ba5ef52dff052a056a5be
WooJunGyu <dnwnsrb11@naver.com>
2025-01-24 15:45:07 +0900

    전체적인 리스트 내부 아이템들 배치 완료

--- src/components/mainPages/community/CommunityList.jsx
+++ src/components/mainPages/community/CommunityList.jsx
@@ -9,18 +9,24 @@ export const CommunityList = () => {
           {/* 제목 */}
           <h2>질문이 있습니다</h2>
         </div>
-        <div className="flex">
+        <div className="flex justify-between">
           {/* 처음 */}
-          <div>
-            <p>이모티콘으로 부자가 되는 법 12월 수업</p>
+          <div className="flex gap-3">
+            <div>
+              <p>이모티콘으로 부자가 되는 법 12월 수업</p>
+            </div>
+            <div>
+              <p>2025년 1월 20일</p>
+            </div>
+            <div>
+              <p>답변완료</p>
+            </div>
           </div>
           <div>
-            <p>2025년 1월 20일</p>
+            <div>
+              <p>크와와와왕</p>
+            </div>
           </div>
-          <div>
-            <p>답변완료</p>
-          </div>
-          <div>{/* 뒤에 */}</div>
         </div>
       </div>
     </>
