9e186565a74cb180d712474d6e4ee2bd2a09cf03
WooJunGyu <dnwnsrb11@naver.com>
2025-01-24 14:05:27 +0900

    feat: 강의 리스트 컴포넌트 제작

--- src/routes/pages/MainPageCategory.jsx
+++ src/routes/pages/MainPageCategory.jsx
@@ -1,7 +1,10 @@
 import { Banner } from '@/components/mainPages/home/Banner';
 import { CategoryList } from '@/components/mainPages/home/category/CategoryList';
-
+import { StructorList } from '@/components/mainPages/home/category/LectureList';
+import { useState } from 'react';
 export const MainPageCategory = () => {
+  // 반복용으로 나둔 요소 추후 변경 예정
+  const [count, setCount] = useState([1, 2, 3, 4]);
   return (
     <>
       <div>
@@ -14,7 +17,18 @@ export const MainPageCategory = () => {
             <CategoryList />
           </div>
         </div>
-        <div>{/* 목록 */}</div>
+        <div className="flex flex-col items-end pb-2 pt-[40px]">
+          <div>
+            <select className="h-10 rounded-md border border-gray-border-color p-2 pl-3 text-[#afafaf]">
+              <option value="recommendation">추천순</option>
+              <option value="petName">최신순</option>
+            </select>
+          </div>
+        </div>
+        <hr />
+        <div>
+          <StructorList />
+        </div>
       </div>
     </>
   );
