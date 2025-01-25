7d945a85e1a439796f4d60b7ba59e85f43c60453
WJG <dnwnsrb11@naver.com>
2025-01-25 18:23:10 +0900

    feat:버튼 클릭시 클래스명 변경 기능 추가

--- src/components/lecture/LectureCategory.jsx
+++ src/components/lecture/LectureCategory.jsx
@@ -1,16 +1,48 @@
+import { useState } from 'react';
+
 export const LectureCategory = () => {
-  const liTextactive = 'text-[22px] font-bold';
-  const liTextDeactive = 'text-[22px] font-light';
+  const liTextactive = 'text-[22px] font-bold cursor-pointer';
+  const liTextDeactive = 'text-[22px] font-light text-[#828282] cursor-pointer';
+
+  const [categoryActive, setCategoryActive] = useState(0);
+  const clickCateogry = (num) => {
+    setCategoryActive(num);
+  };
 
   return (
     <>
       <div>
-        <ul className="flex gap-[30px]">
-          <li>강의 소개</li>
-          <li>커리큘럼</li>
-          <li>다시보기</li>
-          <li>공지사항</li>
-          <li>질문사항</li>
+        <ul className="flex gap-[40px]">
+          <li
+            className={`${categoryActive === 0 ? liTextactive : liTextDeactive}`}
+            onClick={() => setCategoryActive(0)}
+          >
+            강의 소개
+          </li>
+          <li
+            className={`${categoryActive === 1 ? liTextactive : liTextDeactive}`}
+            onClick={() => setCategoryActive(1)}
+          >
+            커리큘럼
+          </li>
+          <li
+            className={`${categoryActive === 2 ? liTextactive : liTextDeactive}`}
+            onClick={() => setCategoryActive(2)}
+          >
+            다시보기
+          </li>
+          <li
+            className={`${categoryActive === 3 ? liTextactive : liTextDeactive}`}
+            onClick={() => setCategoryActive(3)}
+          >
+            공지사항
+          </li>
+          <li
+            className={`${categoryActive === 4 ? liTextactive : liTextDeactive}`}
+            onClick={() => setCategoryActive(4)}
+          >
+            질문사항
+          </li>
         </ul>
       </div>
       <hr />
