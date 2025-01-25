e90d1dc3b67dd65d73c9311a0ab57fd46f2ccb82
woojg <woojg@woojgs-MacBook-Air.local>
2025-01-25 15:42:36 +0900

    feat:강의 페이지 생성

---
+++ src/routes/pages/LecturePage.jsx
@@ -0,0 +1,12 @@
+export const LecturePage = () => {
+  return (
+    <>
+      <div>
+        <div>{/* 배너 */}</div>
+        <div>{/* 강의 인포 */}</div>
+        <div>{/* 탭 */}</div>
+        <div>{/* 컴포넌트 구역 */}</div>
+      </div>
+    </>
+  );
+};
