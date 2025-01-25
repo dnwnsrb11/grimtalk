927f3acf5f643a78f128fdf8d5c8327292c0d091
WJG <dnwnsrb11@naver.com>
2025-01-25 18:17:03 +0900

    feat:카테고리 뼈대 작업

--- src/components/lecture/LectureCategory.jsx
+++ src/components/lecture/LectureCategory.jsx
@@ -1,8 +1,11 @@
 export const LectureCategory = () => {
+  const liTextactive = 'text-[22px] font-bold';
+  const liTextDeactive = 'text-[22px] font-light';
+
   return (
     <>
       <div>
-        <ul>
+        <ul className="flex gap-[30px]">
           <li>강의 소개</li>
           <li>커리큘럼</li>
           <li>다시보기</li>
