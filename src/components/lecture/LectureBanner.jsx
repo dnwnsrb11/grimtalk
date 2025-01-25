c175a1589bd412d1c2e8886374d41063693958b2
WJG <dnwnsrb11@naver.com>
2025-01-25 16:15:45 +0900

    desgin:배너 이미지에 그라데이션 효과 적용 1.양옆 아래 총3가지 그라데이션을 중첩하여 올렸음

--- src/components/lecture/LectureBanner.jsx
+++ src/components/lecture/LectureBanner.jsx
@@ -3,8 +3,13 @@ import BannerIMG from '@/assets/banner/lectureBannerIMG.png';
 export const LectureBanner = () => {
   return (
     <>
-      <div>
-        <img src={BannerIMG} alt="BannerIMG" />
+      <div className="relative h-[300px] w-full rounded-xl">
+        <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-r from-white via-transparent to-transparent opacity-100"></div>
+        <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-l from-white via-transparent to-transparent opacity-100"></div>
+        <div className="absolute bottom-[-1%] left-0 h-full w-full bg-gradient-to-t from-white via-transparent to-transparent opacity-100"></div>
+        <div>
+          <img src={BannerIMG} alt="BannerIMG" className="h-full w-full rounded-xl object-cover" />
+        </div>
       </div>
     </>
   );
