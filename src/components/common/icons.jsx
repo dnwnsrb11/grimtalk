b55486172b1ad00af0e1597eb6b9fa5dfccd3173
WJG <dnwnsrb11@naver.com>
2025-01-26 17:26:12 +0900

    feat:즐겨찾기 아이콘 추가 1. 구독 아이콘을 즐겨찾기 아이콘으로 명시를 잘못함 다음 커밋에서 수정 예정

--- src/components/common/icons.jsx
+++ src/components/common/icons.jsx
@@ -67,4 +67,29 @@ const FavoritIcon = ({
   </svg>
 );
 
-export { DefaultBadge, InstructorLogo, StudentLogo, FavoritIcon };
+// 즐겨찾기 아이콘
+const FavoritIcon1 = ({
+  className = '',
+  width = 15,
+  height = 19,
+  stroke = 'black',
+  strokeWidth = 2,
+}) => (
+  <svg
+    width={width}
+    height={height}
+    viewBox="0 0 15 19"
+    fill="none"
+    xmlns="http://www.w3.org/2000/svg"
+  >
+    <path
+      d="M14 18L7.5 13.2778L1 18V2.88889C1 2.38792 1.19566 1.90748 1.54394 1.55324C1.89223 1.19901 2.3646 1 2.85714 1H12.1429C12.6354 1 13.1078 1.19901 13.4561 1.55324C13.8043 1.90748 14 2.38792 14 2.88889V18Z"
+      stroke={stroke}
+      stroke-width={strokeWidth}
+      stroke-linecap="round"
+      stroke-linejoin="round"
+    />
+  </svg>
+);
+
+export { DefaultBadge, InstructorLogo, StudentLogo, FavoritIcon, FavoritIcon1 };
