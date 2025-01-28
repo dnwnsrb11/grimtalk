723a69700cb298d6a16199aa66a662789a7dec43
WJG <dnwnsrb11@naver.com>
2025-01-28 19:50:47 +0900

    desing: 비활성화 리스트 컴포넌트 - 날짜 부분 디자인 완료

--- src/components/lecture/curriculum/CurriculumLectureLIstDeactive.jsx
+++ src/components/lecture/curriculum/CurriculumLectureLIstDeactive.jsx
@@ -7,19 +7,20 @@ export const CurriculumLectureLIstDeactive = () => {
           <h1 className="text-[22px] font-semibold">커리큘럼 1. 이제 시작입니다.</h1>
           <div className="mt-[15px]">
             <p>
-              여러분들과 함께하는 커리큘럼 첫번째 시간 단순히 그림이 아닌 심도 높은 그림 스킬을 높여
-              드리겠습니다! 여러분들과 함께하는 커리큘럼 첫번째 시간 단순히 그림이 아닌 심도 높은
-              그림 스킬을 높여 드리겠습니다!
+              사회적 특수계급의 제도는 인정되지 아니하며, 어떠한 형태로도 이를 창설할 수 없다.
+              대법원과 각급법원의 조직은 법률로 정한다. 누구든지 체포 또는 구속을 당한 때에는 즉시
+              변호인의 조력을 받을 권리를 가진다. 다만, 형사피고인이 스스로 변호인을 구할 수 없을
+              때에는 법률이 정하는 바에 의하여 국가가 변호인을 붙인다.
             </p>
           </div>
         </div>
-        <div className="flex">
+        <div className="mt-[15px] flex gap-3">
           {/* 하단 정보  */}
           <div className="border-'gray-border-color' rounded-full border bg-bg-gray-color px-[10px] py-[5px]">
             <p className="text-[16px] font-semibold">수업예정</p>
           </div>
-          <div>
-            <p>2025.05.15 12:00</p>
+          <div className="rounded-full bg-primary-color px-[10px] py-[5px]">
+            <p className="text-[16px] font-semibold text-white">2025.05.15 12:00</p>
           </div>
         </div>
       </div>
