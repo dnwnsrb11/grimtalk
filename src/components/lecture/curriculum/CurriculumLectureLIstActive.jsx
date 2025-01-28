dd4a7cd2e9e4ee2131255307cc58511ffb4ce2f8
WJG <dnwnsrb11@naver.com>
2025-01-28 19:58:06 +0900

    desgin: 활성화 컴포넌트에 제목 및 내용 디자인 변경 완료

--- src/components/lecture/curriculum/CurriculumLectureLIstActive.jsx
+++ src/components/lecture/curriculum/CurriculumLectureLIstActive.jsx
@@ -4,9 +4,11 @@ export const CurriculumLectureLIstActive = () => {
       <div className="rounded-2xl border border-gray-border-color p-[20px]">
         <div>
           {/* 제목 및 내용 */}
-          <h1 className="text-[22px] font-semibold">커리큘럼 1. 이제 시작입니다.</h1>
+          <h1 className="text-[22px] font-semibold text-replay-disable-btn-font-color">
+            커리큘럼 1. 이제 시작입니다.
+          </h1>
           <div className="mt-[15px] w-[100%]">
-            <p className="w-[85%]">
+            <p className="w-[85%] text-replay-disable-btn-font-color">
               사회적 특수계급의 제도는 인정되지 아니하며, 어떠한 형태로도 이를 창설할 수 없다.
               대법원과 각급법원의 조직은 법률로 정한다. 누구든지 체포 또는 구속을 당한 때에는 즉시
               변호인의 조력을 받을 권리를 가진다. 다만, 형사피고인이 스스로 변호인을 구할 수 없을
