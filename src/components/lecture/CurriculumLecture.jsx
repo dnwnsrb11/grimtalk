bfcefe8bba8903aca537d359f7a53b9c30affc8b
WJG <dnwnsrb11@naver.com>
2025-01-28 19:33:02 +0900

    feat: 커리큘럼 컴포넌트에 비활성화 리스트 컴포넌트 배치 완료 1. 테스트를 위한 부분이 있기에 추후 코드 변경이 있을수 있음

--- src/components/lecture/CurriculumLecture.jsx
+++ src/components/lecture/CurriculumLecture.jsx
@@ -1,9 +1,13 @@
+import { CurriculumLectureLIstDeactive } from './curriculum/CurriculumLectureLIstDeactive';
+
 export const CurriculumLecture = () => {
   return (
     <>
       <div className="mt-[60px]">
         <h1 className="text-[32px] font-bold">커리큘럼</h1>
-        <div className="mt-[10px]"></div>
+        <div className="mt-[10px]">
+          <CurriculumLectureLIstDeactive />
+        </div>
       </div>
     </>
   );
