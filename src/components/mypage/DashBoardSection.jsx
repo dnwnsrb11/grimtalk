944deb57d9861879bd1459d7652cb5f6ac0d74ac
hoonixox <hoonixox@naver.com>
2025-01-28 20:58:10 +0900

    feat: 최근 구독한 강사 section 추가

--- src/components/mypage/DashBoardSection.jsx
+++ src/components/mypage/DashBoardSection.jsx
@@ -1,6 +1,7 @@
 import { DashBoardCard } from '@/components/mypage/DashBoardCard';
 import { RecentLectureCurriculumItem } from '@/components/mypage/RecentLectureCurriculumItem';
 import { UpcomingLectureItem } from '@/components/mypage/UpcomingLectureItem';
+import { HashTagChip } from '@/components/mypage/HashTagChip';
 
 export const DashBoardSection = () => {
   // 임시 데모 데이터
@@ -30,6 +31,12 @@ export const DashBoardSection = () => {
 
   const myHighestSimilarity = 90;
   const myHighestSimilarityLectureTitle = '이모티콘을 배우고 싶은 당신을 위한 강의';
+
+  const recentInstructor = {
+    image: 'https://picsum.photos/200/300', // demo image
+    nickname: '김싸피',
+    memberTag: ['일러스트', '신입환영'],
+  };
 
   const recentLecture = {
     title: '이모티콘을 배우고 싶은 당신을 위한 강의',
@@ -70,7 +77,27 @@ export const DashBoardSection = () => {
         </DashBoardCard>
       </div>
       <div className="grid grid-cols-2 gap-3">
-        <DashBoardCard title="최근 구독한 강사"></DashBoardCard>
+        <DashBoardCard title="최근 구독한 강사">
+          <button>
+            <div className="flex items-center gap-2">
+              <img
+                src={recentInstructor.image}
+                alt="recent-instructor"
+                className="h-[70px] w-[70px] rounded-full"
+              />
+              <div className="flex flex-col items-start">
+                <p className="text-lg font-bold text-common-font-color">
+                  {recentInstructor.nickname}
+                </p>
+                <div className="flex items-center gap-2">
+                  {recentInstructor.memberTag.map((tag) => (
+                    <HashTagChip key={tag} hashTag={tag} />
+                  ))}
+                </div>
+              </div>
+            </div>
+          </button>
+        </DashBoardCard>
         <DashBoardCard title="최근 구독한 강의">
           <RecentLectureCurriculumItem
             title={recentLecture.title}
