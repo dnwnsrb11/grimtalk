302bfa615f7412ab1712a7a568587d643ba8c0ee
WJG <dnwnsrb11@naver.com>
2025-01-28 19:44:46 +0900

    desgin: 비활성화 컴포넌트 리스트 제목,내용 완료

--- src/components/lecture/curriculum/CurriculumLectureLIstDeactive.jsx
+++ src/components/lecture/curriculum/CurriculumLectureLIstDeactive.jsx
@@ -4,11 +4,12 @@ export const CurriculumLectureLIstDeactive = () => {
       <div className="rounded-2xl border border-gray-border-color p-[20px]">
         <div>
           {/* 제목 및 내용 */}
-          <h1>커리큘럼 1. 이제 시작입니다.</h1>
-          <div>
+          <h1 className="text-[22px] font-semibold">커리큘럼 1. 이제 시작입니다.</h1>
+          <div className="mt-[15px]">
             <p>
               여러분들과 함께하는 커리큘럼 첫번째 시간 단순히 그림이 아닌 심도 높은 그림 스킬을 높여
-              드리겠습니다!
+              드리겠습니다! 여러분들과 함께하는 커리큘럼 첫번째 시간 단순히 그림이 아닌 심도 높은
+              그림 스킬을 높여 드리겠습니다!
             </p>
           </div>
         </div>
