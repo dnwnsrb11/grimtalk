e596e697228187ddeed5e19af49c61a1d2ae4094
hoonixox <hoonixox@naver.com>
2025-01-28 20:29:16 +0900

    feat: 대시보드 예정 커리큘럼 섹션에 강의 아이템 렌더링

--- src/components/mypage/DashBoardSection.jsx
+++ src/components/mypage/DashBoardSection.jsx
@@ -1,7 +1,28 @@
+import { useState } from 'react';
 import { DashBoardCard } from '@/components/mypage/DashBoardCard';
 import { RecentLectureItem } from '@/components/mypage/RecentLectureItem';
+import { UpcomingLectureItem } from '@/components/mypage/UpcomingLectureItem';
 
 export const DashBoardSection = () => {
+  // api로 예정 커리큘럼 불러올 예정
+  const [upcomingLectureList, setUpcomingLectureList] = useState([
+    {
+      title: '이모티콘을 배우고 싶은 당신을 위한 강의',
+      image: 'https://picsum.photos/200/300', // demo image
+      date: '2025-02-01',
+    },
+    {
+      title: '이모티콘을 배우고 싶은 당신을 위한 강의',
+      image: 'https://picsum.photos/200/300', // demo image
+      date: '2025-02-01',
+    },
+    {
+      title: '이모티콘을 배우고 싶은 당신을 위한 강의',
+      image: 'https://picsum.photos/200/300', // demo image
+      date: '2025-02-01',
+    },
+  ]);
+
   return (
     <div className="grid grid-rows-[2fr_1fr_2fr] gap-3">
       <div className="grid grid-cols-2 gap-3">
@@ -15,7 +36,16 @@ export const DashBoardSection = () => {
           </DashBoardCard>
           <DashBoardCard title="나의 가장 높은 유사도"></DashBoardCard>
         </div>
-        <DashBoardCard title="예정 커리큘럼"></DashBoardCard>
+        <DashBoardCard title="예정 커리큘럼">
+          {upcomingLectureList.map((lecture) => (
+            <UpcomingLectureItem
+              key={lecture.title}
+              title={lecture.title}
+              image={lecture.image}
+              date={lecture.date}
+            />
+          ))}
+        </DashBoardCard>
       </div>
       <div className="grid grid-cols-2 gap-3">
         <DashBoardCard title="최근 구독한 강사"></DashBoardCard>
