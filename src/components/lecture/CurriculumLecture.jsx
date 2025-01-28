ee639f7f03e0dab78046d2ea9975534a9580d4ca
WJG <dnwnsrb11@naver.com>
2025-01-28 20:11:06 +0900

    test: 활성화 비활성화 여부에 따라 다른 컴포넌트를 랜더링 1. test이기에 추후 변경 예정

--- src/components/lecture/CurriculumLecture.jsx
+++ src/components/lecture/CurriculumLecture.jsx
@@ -3,11 +3,12 @@ import { CurriculumLectureLIstDeactive } from './curriculum/CurriculumLectureLIs
 
 export const CurriculumLecture = () => {
   // 받은 값이 ture, false에 따라 출력되는 컴포넌트가 다름
-  const selectListComponents = (boolean) => {
+  //   index 추가
+  const selectListComponents = (boolean, index) => {
     if (boolean === false) {
-      return <CurriculumLectureLIstDeactive />;
+      return <CurriculumLectureLIstDeactive key={index} />;
     } else {
-      return <CurriculumLectureLIstActive />;
+      return <CurriculumLectureLIstActive key={index} />;
     }
   };
   // 테스트를 위한 배열 생성
@@ -18,8 +19,8 @@ export const CurriculumLecture = () => {
       <div className="mt-[60px]">
         <h1 className="text-[32px] font-bold">커리큘럼</h1>
         <div className="mt-[10px] flex flex-col gap-[30px]">
-          <CurriculumLectureLIstDeactive />
-          <CurriculumLectureLIstActive />
+          {test.map((boolean, index) => selectListComponents(boolean, index))}
+          {/* 전달 받은 값에 따라 다른 컴포넌트를 출력 */}
         </div>
       </div>
     </>
