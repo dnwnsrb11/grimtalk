bd745fe31ca062149c98dc634db55f2a02856f2d
WJG <dnwnsrb11@naver.com>
2025-01-29 02:04:19 +0900

    feat:강의페이지에 리스트 컴포넌트 추가

--- src/components/lecture/ReplayLecture.jsx
+++ src/components/lecture/ReplayLecture.jsx
@@ -1,10 +1,12 @@
+import { ReplayLectureList } from './replay/ReplayLectureList';
+
 export const ReplayLecture = () => {
   return (
     <>
       <div className="mt-[60px]">
         <h1 className="text-[32px] font-bold">다시보기</h1>
         <div className="mt-[10px]">
-          <p>hello</p>
+          <ReplayLectureList />
         </div>
       </div>
     </>
