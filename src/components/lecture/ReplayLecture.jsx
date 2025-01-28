bef55cc2cb61002598804d92ed834b8504c37033
WJG <dnwnsrb11@naver.com>
2025-01-29 02:00:33 +0900

    feat:다시보기 리스트 생성

--- src/components/lecture/ReplayLecture.jsx
+++ src/components/lecture/ReplayLecture.jsx
@@ -1,7 +1,12 @@
 export const ReplayLecture = () => {
   return (
     <>
-      <div></div>
+      <div className="mt-[60px]">
+        <h1 className="text-[32px] font-bold">다시보기</h1>
+        <div className="mt-[10px]">
+          <p>hello</p>
+        </div>
+      </div>
     </>
   );
 };
---
+++ src/components/lecture/replay/ReplayLectureList.jsx
@@ -0,0 +1,10 @@
+export const ReplayLectureList = () => {
+  return (
+    <>
+      <div>
+        <div>{/* 왼쪽 */}</div>
+        <div>{/* 오른쪽 */}</div>
+      </div>
+    </>
+  );
+};
