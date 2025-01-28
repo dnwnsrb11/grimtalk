df5613d14276c7f2ba5f1b1b59e685449ae7a1a6
WJG <dnwnsrb11@naver.com>
2025-01-28 20:06:58 +0900

    feat: true, false에 따라 출력하는 컴포넌트를 다르게 설정하는 함수 작성d

--- src/components/lecture/CurriculumLecture.jsx
+++ src/components/lecture/CurriculumLecture.jsx
@@ -2,6 +2,15 @@ import { CurriculumLectureLIstActive } from './curriculum/CurriculumLectureLIstA
 import { CurriculumLectureLIstDeactive } from './curriculum/CurriculumLectureLIstDeactive';
 
 export const CurriculumLecture = () => {
+  // 받은 값이 ture, false에 따라 출력되는 컴포넌트가 다름
+  const selectListComponents = (boolean) => {
+    if (boolean === false) {
+      return <CurriculumLectureLIstDeactive />;
+    } else {
+      return <CurriculumLectureLIstActive />;
+    }
+  };
+
   return (
     <>
       <div className="mt-[60px]">
