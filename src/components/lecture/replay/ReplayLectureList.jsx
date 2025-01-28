0575cefac5509d385c69125975f3088ea3083eba
WJG <dnwnsrb11@naver.com>
2025-01-29 02:17:49 +0900

    desgin: 왼쪽 정보 디자인 완료

--- src/components/lecture/replay/ReplayLectureList.jsx
+++ src/components/lecture/replay/ReplayLectureList.jsx
@@ -1,26 +1,28 @@
 export const ReplayLectureList = () => {
   return (
     <>
-      <div className="rounded-2xl border border-gray-border-color bg-bg-gray-color p-[20px]">
-        <div>
+      <div className="flex w-full rounded-2xl border border-gray-border-color bg-bg-gray-color p-[20px]">
+        <div className="w-[80%]">
           {/* 왼쪽 */}
           <div>
-            <h1 className="text-[22px] font-semibold text-text-gray-color">
-              커리큘럼 1. 진짜 시작
-            </h1>
-            <p className="mt-[15px] text-text-gray-color">커리큘럼 테스트 작성 구역</p>
-          </div>
-          <div>
-            {/* 하단 정보 */}
             <div>
-              <p>수업완료</p>
+              <h1 className="text-[22px] font-semibold text-text-gray-color">
+                커리큘럼 1. 진짜 시작
+              </h1>
+              <p className="mt-[15px] text-text-gray-color">커리큘럼 테스트 작성 구역</p>
             </div>
-            <div>
-              <p>2025.03.01</p>
+            <div className="mt-[15px] flex gap-3">
+              {/* 하단 정보 */}
+              <div className="rounded-full bg-black px-[10px] py-[5px] ">
+                <p className="text-[14px] font-semibold text-white">수업완료</p>
+              </div>
+              <div className="rounded-full bg-black px-[10px] py-[5px] ">
+                <p className="text-[14px] font-semibold text-white">2025.03.01</p>
+              </div>
             </div>
           </div>
         </div>
-        <div className="border-l border-gray-border-color">
+        <div className="w-[20%] border-l border-gray-border-color">
           {/* 오른쪽 */}
           <div>
             <div>
