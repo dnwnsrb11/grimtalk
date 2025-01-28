79dbec2e8a5bb9b4da0aaf3e3ba3bd8ff016f650
hoonixox <hoonixox@naver.com>
2025-01-28 20:29:02 +0900

    feat: 마이페이지 예정 강의 아이템 컴포넌트 추가

---
+++ src/components/mypage/UpcomingLectureItem.jsx
@@ -0,0 +1,20 @@
+import { DateChip } from '@/components/mypage/DateChip';
+export const UpcomingLectureItem = ({ title, image, date }) => {
+  return (
+    // 커리큘럼 이미지가 70px이기 때문에 컬럼 너비를 70px로 고정
+    <div className="mb-3 grid grid-cols-[70px_1fr_110px] items-center justify-between gap-2">
+      <img
+        src={image}
+        alt="lecture-curriculum"
+        className="h-[70px] w-[70px] flex-shrink-0 rounded-xl"
+      />
+
+      <div className="flex flex-col items-start truncate">
+        <p className="w-full truncate text-lg font-bold">{title}</p>
+        <DateChip date={date} />
+      </div>
+
+      <button className="text-lg font-semibold text-detail-text-color">자세히 보기 ＞</button>
+    </div>
+  );
+};
