679f9c5ee583a4b5996727e42f4a1a3f7ee17b26
hoonixox <hoonixox@naver.com>
2025-01-28 20:48:08 +0900

    design: DashBoardCard 타이틀 마진 조건부 적용

--- src/components/mypage/DashBoardCard.jsx
+++ src/components/mypage/DashBoardCard.jsx
@@ -1,7 +1,7 @@
 export const DashBoardCard = ({ title, subtitle, children }) => {
   return (
     <div className="rounded-2xl border border-gray-border-color px-4 py-5">
-      <p className="text-[22px] font-bold">{title}</p>
+      <p className={`text-[22px] font-bold ${!subtitle && 'mb-3'}`}>{title}</p>
       {subtitle && (
         <p className="mb-3 truncate text-lg font-bold text-common-font-color opacity-70">
           {subtitle}
