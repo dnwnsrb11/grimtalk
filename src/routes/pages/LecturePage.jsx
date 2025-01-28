6927ccaa63ea74f33b7a30ed57169ea969d39639
WJG <dnwnsrb11@naver.com>
2025-01-28 18:57:42 +0900

    chore:콘솔 명령어 제거

--- src/routes/pages/LecturePage.jsx
+++ src/routes/pages/LecturePage.jsx
@@ -10,7 +10,6 @@ export const LecturePage = () => {
   //   자식으로 부터 값을 받기 위한 함수
   const handleCatagory = (childData) => {
     setCatagory(childData);
-    console.log(childData);
   };
   // 여기에 해당하는 컴포넌트를 저장한다.
   // 자식 컴포넌트인 LectureCategory에서 값을 받아 catagory에 값을 넣어주면 값이 변경되어 아래에 다른 컴포넌트가 리랜더링 된다.
