fc349b2eea1228ee368c08f8f12bcf713439c73e
WJG <dnwnsrb11@naver.com>
2025-01-29 02:02:20 +0900

    feat: 오른쪽 정보 작성

--- src/components/lecture/replay/ReplayLectureList.jsx
+++ src/components/lecture/replay/ReplayLectureList.jsx
@@ -2,7 +2,22 @@ export const ReplayLectureList = () => {
   return (
     <>
       <div>
-        <div>{/* 왼쪽 */}</div>
+        <div>
+          {/* 왼쪽 */}
+          <div>
+            <h1>커리큘럼 1. 진짜 시작</h1>
+            <p>커리큘럼 테스트 작성 구역</p>
+          </div>
+          <div>
+            {/* 하단 정보 */}
+            <div>
+              <p>수업완료</p>
+            </div>
+            <div>
+              <p>2025.03.01</p>
+            </div>
+          </div>
+        </div>
         <div>{/* 오른쪽 */}</div>
       </div>
     </>
