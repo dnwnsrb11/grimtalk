ec9e0058976f3f80198708f81926ac1430f689ae
WJG <dnwnsrb11@naver.com>
2025-01-28 19:35:14 +0900

    feat: 비활성화 리스트 컴포넌트에 내용 구조화

--- src/components/lecture/curriculum/CurriculumLectureLIstDeactive.jsx
+++ src/components/lecture/curriculum/CurriculumLectureLIstDeactive.jsx
@@ -1,7 +1,22 @@
 export const CurriculumLectureLIstDeactive = () => {
   return (
     <>
-      <div></div>
+      <div>
+        <div>
+          {/* 제목 및 내용 */}
+          <h1>커리큘럼 1. 이제 시작입니다.</h1>
+          <div>
+            <p>
+              여러분들과 함께하는 커리큘럼 첫번째 시간 단순히 그림이 아닌 심도 높은 그림 스킬을 높여
+              드리겠습니다!
+            </p>
+          </div>
+        </div>
+        <div>
+          {/* 하단 정보  */}
+          <div></div>
+        </div>
+      </div>
     </>
   );
 };
