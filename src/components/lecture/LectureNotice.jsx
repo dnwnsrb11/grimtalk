d63f60a4a33d5e3a90fab81543abfc913116e0fe
WJG <dnwnsrb11@naver.com>
2025-01-29 15:53:49 +0900

    refector: 마진 높이 조절 1. 10 -> 40

--- src/components/lecture/LectureNotice.jsx
+++ src/components/lecture/LectureNotice.jsx
@@ -6,7 +6,7 @@ export const LectureNotice = () => {
       <div className="mt-[60px]">
         <h1 className="text-[32px] font-bold">공지사항</h1>
         {/* 공지사항 내용 */}
-        <div className="mt-[10px]">
+        <div className="mt-[40px]">
           <LectureNoticeCard />
         </div>
       </div>
