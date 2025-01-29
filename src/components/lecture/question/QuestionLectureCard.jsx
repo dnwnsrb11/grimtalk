bdf2e4e3fdd8c95795d7f20323d31484178d6f89
WJG <dnwnsrb11@naver.com>
2025-01-29 16:33:37 +0900

    refector: flex 구조화 변경 -> 굳이 div를 밖으로 배치할 필요성이 없이 between을 사용하기로 하였음

--- src/components/lecture/question/QuestionLectureCard.jsx
+++ src/components/lecture/question/QuestionLectureCard.jsx
@@ -7,25 +7,26 @@ export const QuestionLectureCard = (isActive) => {
           <div>
             <h1 className="text-[22px] font-semibold ">질문이 있습니다.</h1>
           </div>
-          <div>
-            <div>
-              <p>이모티콘으로 부자가 되는 법</p>
+          <div className="flex justify-between">
+            <div className="flex gap-2">
+              <div>
+                <p>이모티콘으로 부자가 되는 법</p>
+              </div>
+              <div>
+                <p>2025.05.21</p>
+              </div>
+              <div>
+                <p>해결</p>
+              </div>
             </div>
             <div>
-              <p>2025.05.21</p>
-            </div>
-            <div>
-              <p>해결</p>
+              <div>
+                <div>
+                  <p>크와와와왕</p>
+                </div>
+              </div>
             </div>
           </div>
-        </div>
-        <div>
-          <div>
-            <div>
-              <p>크와와와왕</p>
-            </div>
-          </div>
-          {/* 오른쪽 */}
         </div>
       </div>
     </>
