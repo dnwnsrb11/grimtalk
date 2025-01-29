b9452fd8f473602c433352227c1a18c0874519ee
WJG <dnwnsrb11@naver.com>
2025-01-29 16:14:55 +0900

    feat: 질문하기 버튼 생성

--- src/components/lecture/LectureQuestions.jsx
+++ src/components/lecture/LectureQuestions.jsx
@@ -1,7 +1,17 @@
 export const LectureQuestions = () => {
   return (
     <>
-      <div></div>
+      <div className="mt-[60px]">
+        <h1 className="text-[32px] font-bold">질문하기</h1>
+        <div>
+          <button>
+            <p>질문하기</p>
+          </button>
+        </div>
+        <div className="mt-[40px]">
+          {/* 강의 소개는 임시 텍스트로 변수에 넣어 작성하였습니다. */}
+        </div>
+      </div>
     </>
   );
 };
