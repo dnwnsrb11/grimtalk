1dc010d06a851cda930d168e6c4b2c67c4128c5f
WJG <dnwnsrb11@naver.com>
2025-01-29 12:41:16 +0900

    comment: 활성화 비활성화 버튼에 관련하여 주석 달아 두었습니다.

--- src/components/lecture/replay/ReplayLectureList.jsx
+++ src/components/lecture/replay/ReplayLectureList.jsx
@@ -1,8 +1,10 @@
 import { truncateText } from '@/components/common/truncateText';
 
+// 강의 다시보기 비활성화시 경고창
 const confirmRewatch = () => {
   alert('현재 다시보기가 활성화가 안되어 있습니다.');
 };
+// 강의 버튼 활성화
 const ReplayButtonActive = () => {
   return (
     <button className="w-full rounded-2xl border bg-primary-color py-[10px] text-center">
@@ -10,6 +12,7 @@ const ReplayButtonActive = () => {
     </button>
   );
 };
+// 강의 버튼 비활성화
 const ReplayButtonDeActive = () => {
   return (
     <button
