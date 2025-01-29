85a96e3526176ba334dfb8e682bf6e18e6385883
WJG <dnwnsrb11@naver.com>
2025-01-29 16:40:51 +0900

    feat: 버튼은 하나의 컴포넌트에서 관리하기로 하였음

--- src/components/lecture/question/QuestionLectureCard.jsx
+++ src/components/lecture/question/QuestionLectureCard.jsx
@@ -1,28 +1,44 @@
+const QuestionActive = () => {
+  return (
+    <div className="rounded-full border border-gray-border-color bg-primary-color px-[15px] py-[5px]">
+      <p className="text-[14px] font-semibold text-white">해결</p>
+    </div>
+  );
+};
+
+const QuestionDeActive = () => {
+  return (
+    <div className="rounded-full border border-gray-border-color bg-primary-color px-[15px] py-[5px]">
+      <p className="text-[14px] font-semibold text-white">해결</p>
+    </div>
+  );
+};
+
 export const QuestionLectureCard = (isActive) => {
   return (
     <>
-      <div className="rounded-2xl border border-gray-border-color px-[20px] py-[15px]">
+      <div className="cursor-pointer rounded-2xl border border-gray-border-color px-[20px] py-[15px]">
         <div>
           {/* 왼쪽 */}
           <div>
             <h1 className="text-[22px] font-semibold ">질문이 있습니다.</h1>
           </div>
-          <div className="flex justify-between">
+          <div className="mt-[12px] flex justify-between">
             <div className="flex gap-2">
-              <div>
-                <p>이모티콘으로 부자가 되는 법</p>
+              <div className="rounded-full border border-gray-border-color bg-bg-gray-color px-[15px] py-[5px]">
+                <p className="text-[14px] font-semibold">이모티콘으로 부자가 되는 법</p>
               </div>
-              <div>
-                <p>2025.05.21</p>
+              <div className="rounded-full border border-gray-border-color bg-bg-gray-color px-[15px] py-[5px]">
+                <p className="text-[14px] font-semibold">2025.05.21</p>
               </div>
-              <div>
-                <p>해결</p>
+              <div className="rounded-full border border-gray-border-color bg-primary-color px-[15px] py-[5px]">
+                <p className="text-[14px] font-semibold text-white">해결</p>
               </div>
             </div>
             <div>
               <div>
-                <div>
-                  <p>크와와와왕</p>
+                <div className="rounded-full border border-gray-border-color px-[15px] py-[5px]">
+                  <p className="text-[14px] font-semibold">크와와와왕</p>
                 </div>
               </div>
             </div>
