49de683c8667563270ecb1cef51f471485556359
WJG <dnwnsrb11@naver.com>
2025-01-29 13:39:58 +0900

    feat:isActive 값에 따라 다르게 랜더링 되도록 구성 완료

--- src/components/lecture/replay/ReplayLectuecheckImageSimilarityButton.jsx
+++ src/components/lecture/replay/ReplayLectuecheckImageSimilarityButton.jsx
@@ -2,9 +2,17 @@ export const ReplayLectuecheckImageSimilarityButton = ({
   isActive,
   imageScore = '그림 비교하기',
 }) => {
-  return (
-    <>
-      <div></div>
-    </>
-  );
+  if (isActive) {
+    return (
+      <div className="w-full rounded-2xl border bg-primary-color py-[10px] text-center">
+        <p className="text-[18px] font-semibold text-white">{imageScore}</p>
+      </div>
+    );
+  } else {
+    return (
+      <div className="w-full rounded-2xl border bg-primary-color py-[10px] text-center">
+        <p className="text-[18px] font-semibold text-white">{imageScore}</p>
+      </div>
+    );
+  }
 };
