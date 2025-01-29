902a8fb817c1fa4587dd82e5086fb0624d07a773
WJG <dnwnsrb11@naver.com>
2025-01-29 13:34:40 +0900

    comment: 주석 추가

--- src/components/lecture/replay/ReplayLectureList.jsx
+++ src/components/lecture/replay/ReplayLectureList.jsx
@@ -1,30 +1,6 @@
 import { truncateText } from '@/components/common/truncateText';
+// 강의 다시보기 버튼
 import { ReplayLectureListPlayButton } from './ReplayLectureListPlayButton';
-
-
-// // 강의 다시보기 비활성화시 경고창
-// const confirmRewatch = () => {
-//   alert('현재 다시보기가 활성화가 안되어 있습니다.');
-// };
-// // 강의 버튼 활성화
-// const ReplayButtonActive = () => {
-//   return (
-//     <button className="w-full rounded-2xl border bg-primary-color py-[10px] text-center">
-//       <p className="text-[18px] font-semibold text-white">강의 다시보기</p>
-//     </button>
-//   );
-// };
-// // 강의 버튼 비활성화
-// const ReplayButtonDeActive = () => {
-//   return (
-//     <button
-//       className="w-full rounded-2xl border bg-[#C1C1C1] py-[10px] text-center"
-//       onClick={() => confirmRewatch()}
-//     >
-//       <p className="text-[18px] font-semibold text-[#969696]">강의 다시보기</p>
-//     </button>
-//   );
-// };
 
 export const ReplayLectureList = () => {
   const testText =
@@ -56,7 +32,7 @@ export const ReplayLectureList = () => {
         <div className="ml-[25px] w-[20%] border-l border-gray-border-color">
           {/* 오른쪽 */}
           <div className="flex h-full w-full flex-col items-center justify-center gap-2 px-[40px]">
-            <ReplayButtonActive />
+            <ReplayLectureListPlayButton isActive={true} />
             <div className="w-full rounded-2xl border bg-primary-color py-[10px] text-center">
               <p className="text-[18px] font-semibold text-white">65%</p>
             </div>
