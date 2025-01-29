c5da096a2aa47eaeb1f398009ca859580792ba69
WJG <dnwnsrb11@naver.com>
2025-01-29 11:30:47 +0900

    rename: 변수 명 변경 isActive로 변경하였습니다.

--- src/components/lecture/CurriculumLecture.jsx
+++ src/components/lecture/CurriculumLecture.jsx
@@ -4,8 +4,8 @@ import { CurriculumLectureCardDeActive } from './curriculum/CurriculumLectureCar
 export const CurriculumLecture = () => {
   // 받은 값이 ture, false에 따라 출력되는 컴포넌트가 다름
   //   index 추가
-  const selectListComponent = (boolean, index) => {
-    if (boolean === false) {
+  const selectListComponent = (isActive, index) => {
+    if (isActive === false) {
       return <CurriculumLectureCardDeActive key={index} />;
     } else {
       return <CurriculumLectureCardActive key={index} />;
@@ -19,7 +19,7 @@ export const CurriculumLecture = () => {
       <div className="mt-[60px]">
         <h1 className="text-[32px] font-bold">커리큘럼</h1>
         <div className="mt-[10px] flex flex-col gap-[30px]">
-          {test.map((boolean, index) => selectListComponent(boolean, index))}
+          {test.map((isActive, index) => selectListComponent(isActive, index))}
           {/* 전달 받은 값에 따라 다른 컴포넌트를 출력 */}
         </div>
       </div>
