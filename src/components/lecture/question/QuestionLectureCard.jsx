3094e96d60f5f3e0aca2d44945cbd88a9f34f99f
WJG <dnwnsrb11@naver.com>
2025-01-29 16:28:00 +0900

    feat: 질문 카드 컴포넌트 구조화

--- src/components/lecture/question/QuestionLectureCard.jsx
+++ src/components/lecture/question/QuestionLectureCard.jsx
@@ -1,7 +1,33 @@
 export const QuestionLectureCard = (isActive) => {
   return (
     <>
-      <div></div>
+      <div>
+        <div>
+          {/* 왼쪽 */}
+          <div>
+            <h1>질문이 있습니다.</h1>
+          </div>
+          <div>
+            <div>
+              <p>이모티콘으로 부자가 되는 법</p>
+            </div>
+            <div>
+              <p>2025.05.21</p>
+            </div>
+            <div>
+              <p>해결</p>
+            </div>
+          </div>
+        </div>
+        <div>
+          <div>
+            <div>
+              <p>크와와와왕</p>
+            </div>
+          </div>
+          {/* 오른쪽 */}
+        </div>
+      </div>
     </>
   );
 };
