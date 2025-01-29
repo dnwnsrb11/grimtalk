b70ac716d0f0d5fba14b2dcad3d57dd7f2de97b6
WJG <dnwnsrb11@naver.com>
2025-01-29 13:31:30 +0900

    feat: 버튼을 다른 컴포넌트 생성하여 사용 1. 이 버튼 이외에도 이미지 유사도 버튼의 활성화 비활성화 기능이 있어 따로 관리하도록 수정하였습니다.

---
+++ src/components/lecture/replay/ReplayLectureListPlayButton.jsx
@@ -0,0 +1,25 @@
+// 강의 다시보기 비활성화시 경고창
+const confirmRewatch = () => {
+  alert('현재 다시보기가 활성화가 안되어 있습니다.');
+};
+
+export const ReplayLectureListPlayButton = (isActive) => {
+  if (isActive) {
+    // 강의 버튼 활성화
+    return (
+      <button className="w-full rounded-2xl border bg-primary-color py-[10px] text-center">
+        <p className="text-[18px] font-semibold text-white">강의 다시보기</p>
+      </button>
+    );
+  } else {
+    return (
+      // 강의 버튼 비활성화
+      <button
+        className="w-full rounded-2xl border bg-[#C1C1C1] py-[10px] text-center"
+        onClick={() => confirmRewatch()}
+      >
+        <p className="text-[18px] font-semibold text-[#969696]">강의 다시보기</p>
+      </button>
+    );
+  }
+};
