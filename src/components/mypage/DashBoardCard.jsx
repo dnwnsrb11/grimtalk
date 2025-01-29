716163a397bf82df5c9488740a7e400b6dfdda74
hoonixox <hoonixox@naver.com>
2025-01-29 21:41:20 +0900

    feat: DashboardCard 제목 렌더링 조건부 처리
    - title prop이 존재할 때만 제목 섹션 렌더링

--- src/components/mypage/DashBoardCard.jsx
+++ src/components/mypage/DashBoardCard.jsx
@@ -1,12 +1,16 @@
 export const DashboardCard = ({ title, subtitle, subInfo, children }) => {
   return (
     <div className="rounded-2xl border border-gray-border-color px-4 py-5">
-      <div className={`flex items-center gap-2 ${!subtitle && 'mb-3'}`}>
-        <p className="text-[22px] font-bold">{title}</p>
-        {subInfo && (
-          <p className="rounded-full bg-primary-color px-3 py-0 text-base text-white">{subInfo}</p>
-        )}
-      </div>
+      {title && (
+        <div className={`flex items-center gap-2 ${!subtitle && 'mb-3'}`}>
+          <p className="text-[22px] font-bold">{title}</p>
+          {subInfo && (
+            <p className="rounded-full bg-primary-color px-3 py-0 text-base text-white">
+              {subInfo}
+            </p>
+          )}
+        </div>
+      )}
       {subtitle && (
         <p className="mb-3 truncate text-lg font-bold text-common-font-color opacity-70">
           {subtitle}
