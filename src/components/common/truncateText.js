0400e7a6300087b0f32cc52832737e2616b1e6cd
WJG <dnwnsrb11@naver.com>
2025-01-29 12:29:31 +0900

    refactor:기본 텍스트 길이  100 -> 250으로 수정

--- src/components/common/truncateText.js
+++ src/components/common/truncateText.js
@@ -1,4 +1,4 @@
-export const truncateText = (text, maxLength = 100) => {
+export const truncateText = (text, maxLength = 250) => {
   // 기본은 100이지만 언제든지 값을 넣어 길이를 조절 가능
   if (text.length > maxLength) {
     return text.slice(0, maxLength) + '...';
