1117389e25ad125adfa6608e6215581b0652945f
WJG <dnwnsrb11@naver.com>
2025-01-29 15:42:38 +0900

    feat:공지사항 카드 컴포넌트 구조화

--- src/components/lecture/notice/LectureNoticeCard.jsx
+++ src/components/lecture/notice/LectureNoticeCard.jsx
@@ -1,7 +1,10 @@
 export const LectureNoticeCard = () => {
   return (
     <>
-      <div></div>
+      <div>
+        <div>{/* 제목 */}</div>
+        <div>{/* 하단 정보 */}</div>
+      </div>
     </>
   );
 };
