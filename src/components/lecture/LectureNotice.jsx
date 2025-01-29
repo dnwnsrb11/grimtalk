fb1273f5be9940f19066b391fd3cad8f2778ecb3
WJG <dnwnsrb11@naver.com>
2025-01-29 15:34:46 +0900

    feat: 공지사항 구조 완성

--- src/components/lecture/LectureNotice.jsx
+++ src/components/lecture/LectureNotice.jsx
@@ -1,7 +1,11 @@
 export const LectureNotice = () => {
   return (
     <>
-      <div></div>
+      <div className="mt-[60px]">
+        <h1 className="text-[32px] font-bold">공지사항</h1>
+        {/* 공지사항 내용 */}
+        <div className="mt-[10px]"></div>
+      </div>
     </>
   );
 };
