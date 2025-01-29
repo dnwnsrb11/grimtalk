a4fb8aa582bc0e800832ad18ccc6232843b6cbac
WJG <dnwnsrb11@naver.com>
2025-01-29 12:27:39 +0900

    feat:텍스트 길이 조절 기능 작성 1. 너무 긴 텍스트가 들어올시 스타일링이 깨질 우려가 있어 해당 기능을 작성

---
+++ src/components/common/truncateText.js
@@ -0,0 +1,8 @@
+export const truncateText = (text, maxLength = 100) => {
+  // 기본은 100이지만 언제든지 값을 넣어 길이를 조절 가능
+  if (text.length > maxLength) {
+    return text.slice(0, maxLength) + '...';
+  } else {
+    return text;
+  }
+};
