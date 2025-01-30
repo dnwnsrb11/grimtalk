fb1f479dd277792436ad10a7ec35ab15002e21f4
WJG <dnwnsrb11@naver.com>
2025-01-30 19:11:04 +0900

    desing: 내부 내용에 최소 높이 구현 -> 디자인 통일성을 위하여

--- src/components/lecture/replay/ReplayLectureDetail.jsx
+++ src/components/lecture/replay/ReplayLectureDetail.jsx
@@ -2,8 +2,10 @@ export const ReplayLectureDetail = ({ replayDate, setIsActive }) => {
   return (
     <>
       <div className="mt-[60px]">
-        <h1 className="text-[32px] font-bold">커리큘럼 1. 새로운 시작</h1>
-        <p>{replayDate}</p>
+        <div className="min-h-[300px]">
+          <h1 className="text-[32px] font-bold">커리큘럼 1. 새로운 시작</h1>
+          <p className="text-[18px] font-medium">{replayDate}</p>
+        </div>
         <div className="mt-[10px] flex items-center justify-center">
           <button className="rounded-2xl border bg-primary-color px-[120px] py-[20px] text-center">
             <p className="text-[18px] font-semibold text-white">다시보기</p>
