137e53c648862fdeed1f012148a6734f36db91b7
WJG <dnwnsrb11@naver.com>
2025-01-30 22:56:01 +0900

    refector:리뷰 페이지 전체 구조 변경

--- src/components/lecture/LectureReview.jsx
+++ src/components/lecture/LectureReview.jsx
@@ -2,8 +2,11 @@ export const LectureReview = () => {
   return (
     <>
       <div className="mt-[60px]">
-        <h1 className="text-[32px] font-bold">리뷰하기</h1>
-        <div className="mt-[10px]"></div>
+        <div>
+          <div>{/* 별점 */}</div>
+          <div>{/* 리뷰작성 */}</div>
+        </div>
+        <div>{/* 리뷰 모음 */}</div>
       </div>
     </>
   );
