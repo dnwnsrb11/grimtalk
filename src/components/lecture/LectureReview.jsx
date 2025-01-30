d26ff751f8446a05cd33f9fd1ff48aad1e2c267b
WJG <dnwnsrb11@naver.com>
2025-01-30 23:05:24 +0900

    feat: 별점 아이콘 배치

--- src/components/lecture/LectureReview.jsx
+++ src/components/lecture/LectureReview.jsx
@@ -1,9 +1,15 @@
+import { StarReviewIcon } from '@/components/common/icons';
+
 export const LectureReview = () => {
   return (
     <>
       <div className="mt-[60px]">
         <div>
-          <div>{/* 별점 */}</div>
+          <div>
+            {Array.from({ length: 5 }).map((_, index) => (
+              <StarReviewIcon key={index} />
+            ))}
+          </div>
           <div>{/* 리뷰작성 */}</div>
         </div>
         <div>{/* 리뷰 모음 */}</div>
