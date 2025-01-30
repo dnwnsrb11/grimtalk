f2d887f7214e2fc3ef5970ead27f598fe536f4f9
hoonixox <hoonixox@naver.com>
2025-01-30 17:41:19 +0900

    feat: Zustand demo store 추가(주석 참고하여 사용법 숙지 필요)

--- src/routes/pages/DemoPage.jsx
+++ src/routes/pages/DemoPage.jsx
@@ -1,7 +1,27 @@
+import { useDemoStore } from '@/store/useDemoStore';
+
 export const DemoPage = () => {
+  // selector 패턴 사용
+  const bears = useDemoStore((state) => state.bears);
+  const increasePopulation = useDemoStore((state) => state.increasePopulation);
+  const removeAllBears = useDemoStore((state) => state.removeAllBears);
+  const updateBears = useDemoStore((state) => state.updateBears);
+
   return (
     <div className="col-span-14">
       <h1 className="text-2xl font-bold">DemoPage</h1>
+      <div className="flex flex-col items-baseline gap-2">
+        <p>Bears: {bears}</p>
+        <button className="border border-gray-300 px-2 py-1" onClick={increasePopulation}>
+          Increase Population
+        </button>
+        <button className="border border-gray-300 px-2 py-1" onClick={removeAllBears}>
+          Remove All Bears
+        </button>
+        <button className="border border-gray-300 px-2 py-1" onClick={() => updateBears(bears + 1)}>
+          Update Bears
+        </button>
+      </div>
     </div>
   );
 };
