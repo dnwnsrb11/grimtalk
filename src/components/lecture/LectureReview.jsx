c987563462a7e6dafc630a71b3e9484e0cd0835c
WJG <dnwnsrb11@naver.com>
2025-01-30 22:53:04 +0900

    feat:리뷰하기 초기 구조화

--- src/components/lecture/LectureReview.jsx
+++ src/components/lecture/LectureReview.jsx
@@ -1,7 +1,10 @@
 export const LectureReview = () => {
   return (
     <>
-      <div></div>
+      <div className="mt-[60px]">
+        <h1 className="text-[32px] font-bold">리뷰하기</h1>
+        <div className="mt-[10px]"></div>
+      </div>
     </>
   );
 };
