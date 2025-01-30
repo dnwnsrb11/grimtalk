3d019629907df80208f7247bd13b40d0bb2a44e0
WJG <dnwnsrb11@naver.com>
2025-01-30 18:10:59 +0900

    test: 카드 클릭시 상세페이지를 띄우는 기능에 관련하여 테스트

--- src/components/lecture/ReplayLecture.jsx
+++ src/components/lecture/ReplayLecture.jsx
@@ -13,14 +13,14 @@ export const ReplayLecture = () => {
           <h1 className="text-[32px] font-bold">다시보기</h1>
           <div className="mt-[10px]">
             {testList.map((test, index) => (
-              <div onClick={() => setIsActive(!isActive)}>
-                <ReplayLectureCard key={index} test={test} />
+              <div>
+                <ReplayLectureCard key={index} isActive={isActive} setIsActive={setIsActive} />
               </div>
             ))}
           </div>
         </div>
       ) : (
-        <ReplayLectureDetail />
+        <ReplayLectureDetail isActive={isActive} setIsActive={setIsActive} />
       )}
     </>
   );
