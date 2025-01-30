a7661d0c104536939341f69f936ff74a2b65b0d6
WJG <dnwnsrb11@naver.com>
2025-01-31 01:44:07 +0900

    desgin: 리뷰 카드 디자인 완료

--- src/components/lecture/review/ReviewLectureCard.jsx
+++ src/components/lecture/review/ReviewLectureCard.jsx
@@ -4,12 +4,17 @@ export const ReviewLectureCard = () => {
   return (
     <>
       <div className="rounded-2xl border border-gray-border-color px-[20px] py-[35px]">
-        <div>
+        <div className="flex items-center justify-between">
           <div>
             <h1>리뷰 입니다.</h1>
           </div>
-          <div>
+          <div className="flex items-center gap-2">
             <StarReviewIcon />
+            <div>
+              <p className="text-[18px] text-[#828282]">
+                <span className="text-primary-color">4</span> / 5
+              </p>
+            </div>
           </div>
         </div>
       </div>
