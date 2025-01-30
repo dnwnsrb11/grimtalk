4ebd81486b63272319dd4ed305d1a3cfd1c2f29c
WJG <dnwnsrb11@naver.com>
2025-01-30 18:32:47 +0900

    test: 상세페이지 기능 구현

--- src/components/lecture/ReplayLecture.jsx
+++ src/components/lecture/ReplayLecture.jsx
@@ -1,11 +1,16 @@
 import { ReplayLectureCard } from '@/components/lecture/replay/ReplayLectureCard';
 import { ReplayLectureDetail } from './replay/ReplayLectureDetail';
 import { useState } from 'react';
+import { useEffect } from 'react';
 
 export const ReplayLecture = () => {
   // test
   const testList = ['one', 'two', 'three'];
+  const [replayDate, setreplayDate] = useState('');
   const [isActive, setIsActive] = useState(false);
+  useEffect(() => {
+    console.log('값이 변경', replayDate);
+  }, [isActive, replayDate]);
   return (
     <>
       {!isActive ? (
@@ -13,14 +18,24 @@ export const ReplayLecture = () => {
           <h1 className="text-[32px] font-bold">다시보기</h1>
           <div className="mt-[10px]">
             {testList.map((test, index) => (
-              <div>
-                <ReplayLectureCard key={index} isActive={isActive} setIsActive={setIsActive} />
+              <div
+                key={index}
+                onClick={() => {
+                  setIsActive(true);
+                  setreplayDate(test);
+                }}
+              >
+                <ReplayLectureCard test={test} />
               </div>
             ))}
           </div>
         </div>
       ) : (
-        <ReplayLectureDetail isActive={isActive} setIsActive={setIsActive} />
+        <ReplayLectureDetail
+          isActive={isActive}
+          setIsActive={setIsActive}
+          replayDate={replayDate}
+        />
       )}
     </>
   );
--- src/components/lecture/replay/ReplayLectureDetail.jsx
+++ src/components/lecture/replay/ReplayLectureDetail.jsx
@@ -1,10 +1,11 @@
-export const ReplayLectureDetail = () => {
+export const ReplayLectureDetail = ({ replayDate }) => {
   return (
     <>
       <div className="mt-[60px]">
         <h1 className="text-[32px] font-bold">커리큘럼 1. 새로운 시작</h1>
-        <div className="mt-[10px]">
-          <button className="w-full rounded-2xl border bg-primary-color px-[120px] py-[20px] text-center">
+        <p>{replayDate}</p>
+        <div className="mt-[10px] flex items-center justify-center">
+          <button className="rounded-2xl border bg-primary-color px-[120px] py-[20px] text-center">
             <p className="text-[18px] font-semibold text-white">다시보기</p>
           </button>
         </div>
