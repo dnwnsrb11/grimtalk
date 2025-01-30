e6591b795668acc5d333c14e084ae0d7704c9890
WJG <dnwnsrb11@naver.com>
2025-01-30 16:17:01 +0900

    rename: 변수명 변경

--- src/components/lecture/replay/ReplayLectuecheckImageSimilarityButton.jsx
+++ src/components/lecture/replay/ReplayLectuecheckImageSimilarityButton.jsx
@@ -1,7 +1,7 @@
-export const ReplayLectuecheckImageSimilarityButton = ({ imageScore = '그림 비교하기' }) => {
+export const ReplayLectuecheckImageSimilarityButton = ({ similarityPercent = '그림 비교하기' }) => {
   return (
     <button className="w-full rounded-2xl border bg-primary-color py-[10px] text-center">
-      <p className="text-[18px] font-semibold text-white">{imageScore}</p>
+      <p className="text-[18px] font-semibold text-white">{similarityPercent}</p>
     </button>
   );
 };
