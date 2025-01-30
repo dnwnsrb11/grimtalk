01a6bcb44dc25480497a3b33866b4e9bb3af5270
WJG <dnwnsrb11@naver.com>
2025-01-30 16:29:15 +0900

    desing: 강의 다시보기 상세페이지 디자인

--- src/components/lecture/replay/ReplayDetail.jsx
+++ src/components/lecture/replay/ReplayDetail.jsx
@@ -1,7 +1,14 @@
 export const ReplayDetail = () => {
   return (
     <>
-      <div></div>
+      <div className="mt-[60px]">
+        <h1 className="text-[32px] font-bold">커리큘럼 1. 새로운 시작</h1>
+        <div className="mt-[10px]">
+          <button className="w-full rounded-2xl border bg-primary-color px-[120px] py-[20px] text-center">
+            <p className="text-[18px] font-semibold text-white">다시보기</p>
+          </button>
+        </div>
+      </div>
     </>
   );
 };
