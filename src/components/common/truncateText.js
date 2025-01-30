a9349ccb8ea1c2f441b0c8061d53852fbfb7793c
WJG <dnwnsrb11@naver.com>
2025-01-30 16:12:02 +0900

    remove: 기존 js 파일 제거

--- src/components/common/truncateText.js
+++
@@ -1,8 +0,0 @@
-export const truncateText = (text, maxLength = 250) => {
-  // 기본은 100이지만 언제든지 값을 넣어 길이를 조절 가능
-  if (text.length > maxLength) {
-    return text.slice(0, maxLength) + '...';
-  } else {
-    return text;
-  }
-};
