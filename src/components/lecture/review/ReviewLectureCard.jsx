4e50cdce9f322ff550eca34b16be347293dcca90
WJG <dnwnsrb11@naver.com>
2025-01-31 01:22:54 +0900

    feat: 리뷰카드 구조화

--- src/components/lecture/review/ReviewLectureCard.jsx
+++ src/components/lecture/review/ReviewLectureCard.jsx
@@ -1,7 +1,18 @@
+import { StarReviewIcon } from '@/components/common/icons';
+
 export const ReviewLectureCard = () => {
   return (
     <>
-      <div></div>
+      <div className="rounded-2xl border border-gray-border-color px-[20px] py-[35px]">
+        <div>
+          <div>
+            <h1>리뷰 입니다.</h1>
+          </div>
+          <div>
+            <ReviewLectureCard />
+          </div>
+        </div>
+      </div>
     </>
   );
 };
