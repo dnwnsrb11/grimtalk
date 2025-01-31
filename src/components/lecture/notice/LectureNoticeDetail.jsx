9dcd6933eb7a4685a70c7576e5bcf6d6f596c706
WooJunGyu <dnwnsrb11@naver.com>
2025-01-31 13:20:31 +0900

    desgin: 상세페이지 디자인 작업

--- src/components/lecture/notice/LectureNoticeDetail.jsx
+++ src/components/lecture/notice/LectureNoticeDetail.jsx
@@ -1,7 +1,19 @@
 export const LectureNoticeDetail = () => {
   return (
     <>
-      <div></div>
+      <div className="mt-[60px]">
+        <h1 className="text-[32px] font-bold">공지사항 제목목</h1>
+        <div className="mt-[10px]">
+          <p className="text-[18px]">ehllo</p>
+        </div>
+      </div>
+      <hr />
+      <hr className="mt-[40px] border border-divider-color" />
+      <div className="mt-[20px] flex justify-end">
+        <button className="rounded-2xl border border-gray-border-color bg-bg-gray-color p-[10px]">
+          <p className="text-[18px] font-semibold">뒤로가기</p>
+        </button>
+      </div>
     </>
   );
 };
