7ee3a7b31efc06afa8765365fda178a0c8e53c81
WooJunGyu <dnwnsrb11@naver.com>
2025-01-31 15:48:23 +0900

    desgin: 질문사항 상세페이지 디자인 작업

--- src/components/lecture/question/QuestionLectureDetail.jsx
+++ src/components/lecture/question/QuestionLectureDetail.jsx
@@ -1,7 +1,21 @@
 export const QuestionLectureDetail = () => {
   return (
     <>
-      <div></div>
+      <div className="mt-[60px]">
+        <h1 className="text-[32px] font-bold">공지사항 입니다!!</h1>
+        <div className="mt-[10px]">hello</div>
+      </div>
+      <hr />
+      <div className="mt-[60px]">
+        <div>
+          <h1 className="text-[32px] font-bold">답변</h1>
+          <div>
+            <p>답변완료</p>
+          </div>
+        </div>
+        <div className="mt-[10px]">hello</div>
+      </div>
+      <hr />
     </>
   );
 };
