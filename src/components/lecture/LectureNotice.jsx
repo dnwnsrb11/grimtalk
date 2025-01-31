42f071b8d4363ff18180c896ec62a97f5676ed43
WooJunGyu <dnwnsrb11@naver.com>
2025-01-31 14:14:47 +0900

    feat:상세페이지 추가

--- src/components/lecture/LectureNotice.jsx
+++ src/components/lecture/LectureNotice.jsx
@@ -1,8 +1,17 @@
-import { LectureNoticeCard } from './notice/LectureNoticeCard';
+import { useState } from 'react';
+import { LectureNoticeDetail } from '@/components/lecture/notice/LectureNoticeDetail';
+import { LectureNoticeCard } from '@/components/lecture/notice/LectureNoticeCard';
 
 export const LectureNotice = () => {
   // test
   const testList = ['one', 'two', 'three'];
+  // 상세페이지 기능 구현
+  const [noticeDate, setNoticeDate] = useState('');
+  const [isActive, setIsActive] = useState(false);
+
+  if (isActive) {
+    return <LectureNoticeDetail noticeDate={noticeDate} setIsActive={setIsActive} />;
+  }
   return (
     <>
       <div className="mt-[60px]">
@@ -10,7 +19,7 @@ export const LectureNotice = () => {
         {/* 공지사항 내용 */}
         <div className="mt-[40px]">
           {testList.map((testData, index) => (
-            <div key={index} className="mb-3">
+            <div key={index} className="mb-3" onClick={() => setIsActive(true)}>
               <LectureNoticeCard />
             </div>
           ))}
--- src/components/lecture/notice/LectureNoticeDetail.jsx
+++ src/components/lecture/notice/LectureNoticeDetail.jsx
@@ -1,4 +1,7 @@
-export const LectureNoticeDetail = () => {
+export const LectureNoticeDetail = ({ noticeDate, setIsActive }) => {
+  const changeActive = () => {
+    setIsActive(false);
+  };
   return (
     <>
       <div className="mt-[60px]">
@@ -10,7 +13,10 @@ export const LectureNoticeDetail = () => {
       <hr />
       <hr className="mt-[40px] border border-divider-color" />
       <div className="mt-[20px] flex justify-end">
-        <button className="rounded-2xl border border-gray-border-color bg-bg-gray-color p-[10px]">
+        <button
+          className="rounded-2xl border border-gray-border-color bg-bg-gray-color p-[10px]"
+          onClick={() => changeActive()}
+        >
           <p className="text-[18px] font-semibold">뒤로가기</p>
         </button>
       </div>
