7319cc7b972ad2cf0a9ab414469b7593a434dc6b
WooJunGyu <dnwnsrb11@naver.com>
2025-01-31 17:11:15 +0900

    feat: 답변 컴포넌트 정리

--- src/components/lecture/question/QuestionLectureDetail.jsx
+++ src/components/lecture/question/QuestionLectureDetail.jsx
@@ -4,19 +4,23 @@ export const QuestionLectureDetail = ({ setIsActive, testData }) => {
   };
   return (
     <>
-      <div className="mt-[60px]">
+      <div className="mt-[60px] min-h-[200px]">
         <h1 className="text-[32px] font-bold">공지사항 입니다!!</h1>
-        <div className="mt-[10px]">hello</div>
+        <div className="mt-[10px]">
+          <p className="text-[18px]">hello</p>
+        </div>
       </div>
       <hr />
-      <div className="mt-[60px]">
-        <div>
+      <div className="mt-[20px]">
+        <div className="flex gap-3">
           <h1 className="text-[32px] font-bold">답변</h1>
-          <div>
-            <p>답변완료</p>
+          <div className="flex items-center justify-center rounded-full bg-primary-color px-[15px] py-0">
+            <p className="font-semibold text-[14p] text-white">답변완료</p>
           </div>
         </div>
-        <div className="mt-[10px]">hello</div>
+        <div className="mt-[10px] min-h-[100px]">
+          <p className="text-[18px]">hello</p>
+        </div>
       </div>
       <hr />
       <div className="mt-[20px] flex justify-end">
