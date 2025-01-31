54b83fca8b94fcec46cc562b76f9a0130abc1a8e
WooJunGyu <dnwnsrb11@naver.com>
2025-01-31 16:13:40 +0900

    feat:질문 작성 컴포넌트 기초 구조화

--- src/components/lecture/question/QuestionLectureWrite.jsx
+++ src/components/lecture/question/QuestionLectureWrite.jsx
@@ -1,7 +1,13 @@
 export const QuestionLectureWrite = () => {
   return (
     <>
-      <div></div>
+      <div>
+        <div>
+          <input type="text" />
+          <textarea name="" id=""></textarea>
+        </div>
+        <div>{/* 아래 버튼 */}</div>
+      </div>
     </>
   );
 };
