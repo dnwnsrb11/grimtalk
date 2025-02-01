92eb37ceebac4653d0fd9b2d7815602b2838d0eb
woojeonghoon <hoonixox@naver.com>
2025-02-01 13:54:11 +0900

    fix: 대소문자 오타 수정(navbar -> Navbar) - 젠킨스 자동 배포 오류

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -1,6 +1,6 @@
 import { Outlet } from 'react-router-dom';
 
-import { Navbar } from '@/components/navbar/navbar';
+import { Navbar } from '@/components/navbar/Navbar';
 
 export const RootLayout = () => {
   return (
