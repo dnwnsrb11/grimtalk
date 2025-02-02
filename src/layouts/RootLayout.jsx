59995e2845650f8ec36e32e5c591411daab59ca2
hoonixox <hoonixox@naver.com>
2025-02-02 11:53:01 +0900

    design: Navbar 및 RootLayout 구조 개선
    - Navbar 컴포넌트 레이아웃 단순화(grid를 RootLayout에서 관리할 수 있게끔)
    - RootLayout에 Header 섹션 추가(시멘틱)
    - main에 mt-8을 줘서 모든 페이지에서 header와 간격 유지

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,12 +5,16 @@ import { Navbar } from '@/components/layout/Navbar';
 export const RootLayout = () => {
   return (
     <>
-      <Navbar />
       <div className="mx-auto grid min-h-screen max-w-[1920px] grid-cols-14 gap-4">
-        <aside className="col-span-2" aria-hidden="true"></aside>
-        <main className="col-span-10">
-          <Outlet />
-        </main>
+        <div className="col-span-2"></div>
+        <div className="col-span-10">
+          <header className="border-b bg-white">
+            <Navbar />
+          </header>
+          <main className="mt-8">
+            <Outlet />
+          </main>
+        </div>
         <div className="col-span-2"></div>
       </div>
     </>
