08cb2520bf62c0088c5cad1625fc67c7056eb801
hoonixox <hoonixox@naver.com>
2025-02-02 15:25:06 +0900

    design: 구분선 grid에 영향 받지 않도록 디자인 변경

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -5,17 +5,26 @@ import { Navbar } from '@/components/layout/Navbar';
 export const RootLayout = () => {
   return (
     <>
-      <div className="mx-auto grid min-h-screen max-w-[1920px] grid-cols-14 gap-4">
-        <div className="col-span-2"></div>
-        <div className="col-span-10">
-          <header className="border-b bg-white">
-            <Navbar />
-          </header>
-          <main>
-            <Outlet />
-          </main>
+      <div className="mx-auto min-h-screen max-w-[1920px]">
+        <div className="grid grid-cols-14 gap-4">
+          <div className="col-span-2"></div>
+          <div className="col-span-10">
+            <header>
+              <Navbar />
+            </header>
+          </div>
+          <div className="col-span-2"></div>
         </div>
-        <div className="col-span-2"></div>
+        <hr className="w-full border-gray-200" />
+        <div className="grid grid-cols-14 gap-4">
+          <div className="col-span-2"></div>
+          <div className="col-span-10">
+            <main>
+              <Outlet />
+            </main>
+          </div>
+          <div className="col-span-2"></div>
+        </div>
       </div>
     </>
   );
