7458ebeb487021d12c59d66270f51151e605b021
WooJunGyu <dnwnsrb11@naver.com>
2025-02-03 14:13:43 +0900

    feat:404 페이지 완료

--- src/routes/pages/NotfoundPage.jsx
+++ src/routes/pages/NotfoundPage.jsx
@@ -1,7 +1,16 @@
 export const NotfoundPage = () => {
   return (
     <>
-      <div></div>
+      <div className="w-full h-[calc(100vh-50px)] flex justify-center items-center">
+        <div className="flex justify-center items-center flex-col">
+          <div className="font-extrabold text-[200px] ">
+            <span className="text-primary-color">4</span>
+            <span className="text-[#8F00FF]">0</span>
+            <span className="text-[#23C4F7]">4</span>
+          </div>
+          <p className="text-[24px] text-[#838383]">try reload this page or go back to homepage</p>
+        </div>
+      </div>
     </>
   );
 };
