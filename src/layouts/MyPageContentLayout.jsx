bcac7dd6e191ef484a8eca4e45dea637bb056be1
임재열 <dlawoduf15@naver.com>
2025-02-03 14:05:40 +0900

    feat:내가 쓴 글 목록 api연결 - 로그인 로직이 끝나면 마무리 하겠습니다.

--- src/layouts/MyPageContentLayout.jsx
+++ src/layouts/MyPageContentLayout.jsx
@@ -1,4 +1,19 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { _axios } from '@/api/instance';
 export const MyPageContentLayout = ({ navMenuTitle, navMenuSubButton, children }) => {
+  const {
+    data: myquestions,
+    error,
+    isLoading,
+  } = useQuery({
+    queryKey: ['myquestions'],
+    queryFn: async () => {
+      const { data } = await _axios.get(`/mypage/board`);
+      return data;
+    },
+  });
+
   return (
     <>
       <div className="mt-10 flex flex-row items-center gap-2">
