d9a1901d0f3783b6650dfd1beaa624b30040a5d2
WooJunGyu <dnwnsrb11@naver.com>
2025-02-03 13:13:55 +0900

    desgine:로딩 구현 -> 초기 구성

--- src/components/common/LoadingComponents.jsx
+++ src/components/common/LoadingComponents.jsx
@@ -1,7 +1,7 @@
 export const LoadingComponents = () => {
   return (
     <>
-      <div></div>
+      <span className="w-12 h-12 border-4 border-white rounded-full inline-block relative box-border animate-spin after:content-[''] after:absolute after:left-0 after:top-0 after:bg-red-500 after:w-4 after:h-4 after:rounded-full after:transform after:-translate-x-1/2 after:translate-y-1/2"></span>
     </>
   );
 };
