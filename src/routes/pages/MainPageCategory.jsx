d6c02ca4969b9ce24b55141e4f8777fac463b2a4
임재열 <a01092201761@gmail.com>
2025-02-03 21:39:14 +0900

    feat:category api 요청 구현

--- src/routes/pages/MainPageCategory.jsx
+++ src/routes/pages/MainPageCategory.jsx
@@ -1,11 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
+import { useParams } from 'react-router-dom';
 
+import { _axios } from '@/api/instance';
 import { Banner } from '@/components/mainPages/home/Banner';
 import { CategoryList } from '@/components/mainPages/home/category/CategoryList';
 import { LectureItem } from '@/components/mainPages/home/category/LectureItem';
 export const MainPageCategory = () => {
   // 반복용으로 나둔 요소 추후 변경 예정
   const [count, setCount] = useState([1, 2, 3, 4]);
+
+  const { searchQuery } = useParams(); // URL에서 검색어와 페이지 가져오기
+
+  const { data: categorySearch } = useQuery({
+    queryKey: ['categorySearch', searchQuery], // 🔥 페이지네이션 적용
+    queryFn: async () => {
+      if (!searchQuery) return [];
+      const { data } = await _axios.get(`/lecture/search?keyword=${searchQuery}&page=1`);
+      return data;
+    },
+    enabled: !!searchQuery,
+  });
   return (
     <div className="mt-10">
       <Banner />
