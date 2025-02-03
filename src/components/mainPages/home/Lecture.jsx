e4f5c0738a383a4387cbc2ddb1c7eaf3b2b2d2ae
임재열 <dlawoduf15@naver.com>
2025-02-03 15:14:01 +0900

    feat:메인페이지 - 인기 있는 강의 컴포넌트 API 구현

--- src/components/mainPages/home/Lecture.jsx
+++ src/components/mainPages/home/Lecture.jsx
@@ -1,27 +1,30 @@
 import starSVG from '@/assets/banner/star.svg';
-import testImg from '@/assets/banner/Test/TestImg.png';
 
-export const Lecture = () => {
+export const Lecture = ({ index, lecture }) => {
   const score = 4.4;
+  const lectureSubject = lecture.subject;
+  const lectureImg = lecture.image;
+  const lectureNickname = lecture.nickname;
+  const lectureTags = lecture.tags;
+  const lectureCategory = lecture.category ?? null;
+  const lectureStar = lecture.start ?? `0.0`;
+  console.log(lectureSubject);
   return (
     <>
       <div>
         <div className="min-h-[160px] overflow-hidden rounded-lg">
-          <img src={testImg} alt="" className="h-full w-full object-cover" />
+          <img src={lectureImg} alt="" className="h-full w-full object-cover" />
         </div>
         <div>
-          <h4 className="mt-2 text-lg leading-tight">
-            이모티콘을 배오고 싶은 당신을 위한 재밌는 강의
-          </h4>
+          <h4 className="mt-2 text-lg leading-tight">{lectureSubject}</h4>
           <div className="mt-2 flex items-center gap-3 ">
-            <h4 className="text-base font-bold">Name</h4>
+            <h4 className="text-base font-bold">{lectureNickname}</h4>
             <div className="flex gap-1">
-              <div className="inline-block rounded-full border bg-bg-gray-color px-3 py-1">
-                <p className="text-text-gray-color">일러스트</p>
-              </div>
-              <div className="inline-block rounded-full border bg-bg-gray-color px-3 py-1">
-                <p className="text-text-gray-color">캐릭터</p>
-              </div>
+              {lectureTags?.map((tag, index) => (
+                <div className="inline-block rounded-full border bg-bg-gray-color px-3 py-1">
+                  <p className="text-text-gray-color">{tag}</p>
+                </div>
+              ))}
             </div>
           </div>
         </div>
@@ -29,7 +32,7 @@ export const Lecture = () => {
           <div>
             <img src={starSVG} alt="starIcon" />
           </div>
-          <p className="text-text-gray-color">{score} / 5</p>
+          <p className="text-text-gray-color">{lectureStar} / 5</p>
         </div>
       </div>
     </>
