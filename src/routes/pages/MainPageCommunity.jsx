823c3cb94db7f54f90168ec34fe441fd47eb0df1
WooJunGyu <dnwnsrb11@naver.com>
2025-02-03 15:08:36 +0900

    feat:api 추가 성공

--- src/routes/pages/MainPageCommunity.jsx
+++ src/routes/pages/MainPageCommunity.jsx
@@ -1,21 +1,47 @@
+import { _axios } from '@/api/instance';
+import { LoadingComponents } from '@/components/common/LoadingComponents';
 import { CommunityBanner } from '@/components/mainPages/community/CommunityBanner';
 import { CommunityList } from '@/components/mainPages/community/CommunityList';
+import { useQuery } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
 
 export const MainPageCommunity = () => {
-  const countlist = [1, 2, 3, 4];
+  const navigate = useNavigate();
+  // 전체 커뮤니티 목록
+  const {
+    data: communities,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ['communities'],
+    queryFn: async () => {
+      const { data } = await _axios.get('/board');
+      return data.body.data.list;
+    },
+    onError: () => {
+      navigate('/notfound');
+    },
+  });
+  if (isLoading) {
+    return <LoadingComponents />;
+  }
+
   return (
-    <div className="mt-10">
-      <div>
-        <CommunityBanner />
-      </div>
-      <div className="pt-[50px]">
-        <hr />
-        <div className="mt-[10px]">
-          {countlist.map((c, index) => (
-            <CommunityList key={index} />
-          ))}
+    <>
+      <div className="mt-10">
+        <div>
+          <CommunityBanner />
+        </div>
+        <div className="pt-[50px]">
+          <hr />
+          <div className="mt-[10px]">
+            {communities?.map((community, index) => (
+              <CommunityList key={index} community={community} />
+            ))}
+          </div>
         </div>
       </div>
-    </div>
+    </>
   );
 };
