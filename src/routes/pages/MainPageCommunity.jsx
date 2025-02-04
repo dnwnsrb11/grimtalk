8641c5f90b456a7c516e79da880d66f11b0c626e
hoonixox <hoonixox@naver.com>
2025-02-04 18:36:04 +0900

    style: eslint 규칙에 맞게 코드 스타일 수정

--- src/routes/pages/MainPageCommunity.jsx
+++ src/routes/pages/MainPageCommunity.jsx
@@ -1,17 +1,15 @@
+import { useQuery } from '@tanstack/react-query';
+import { useNavigate } from 'react-router-dom';
+
 import { _axios } from '@/api/instance';
 import { LoadingComponents } from '@/components/common/LoadingComponents';
 import { CommunityBanner } from '@/components/mainPages/community/CommunityBanner';
 import { CommunityList } from '@/components/mainPages/community/CommunityList';
-import { useQuery } from '@tanstack/react-query';
-import { useNavigate } from 'react-router-dom';
 
 export const MainPageCommunity = () => {
   const navigate = useNavigate();
   // 전체 커뮤니티 목록
-  const {
-    data: communities,
-    isLoading,
-  } = useQuery({
+  const { data: communities, isLoading } = useQuery({
     queryKey: ['communities'],
     queryFn: async () => {
       const { data } = await _axios.get('/board');
