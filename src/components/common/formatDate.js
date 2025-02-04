df93360b383018576900cdb74689a05baa859952
WooJunGyu <dnwnsrb11@naver.com>
2025-02-04 16:29:01 +0900

    refector: 현재 디자인상 년,월,일 시간 까지 표시가 되는걸로 확인하여 그런 방식으로 수정

--- src/components/common/formatDate.js
+++ src/components/common/formatDate.js
@@ -1,6 +1,13 @@
-const formatDate = (data) => {
-    const dataString = String(data);
-    const datePart = dataString.split('T'); 
-    const formattedDate = datePart[0]?.replaceAll('-', '.') || ''; 
-    return formattedDate;
-  };
+const formatDate = (dateStr) => {
+  const date = new Date(dateStr);
+
+  const year = date.getFullYear();
+  // +1을 제거해야 합니다. getMonth()에만 +1이 필요합니다
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0'); // +1 제거
+  const hours = String(date.getHours()).padStart(2, '0'); // +1 제거
+  const minutes = String(date.getMinutes()).padStart(2, '0'); // +1 제거
+
+  const formatted = `${year}.${month}.${day} ${hours}:${minutes}`;
+  return formatted;
+};
