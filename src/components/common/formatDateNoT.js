3c0658f8bc06da4be2246204af8598cf410aaa05
WooJunGyu <dnwnsrb11@naver.com>
2025-02-04 16:45:53 +0900

    feat:시간을 제외한 년월일이 보니는 경우에 사용하는 함수

---
+++ src/components/common/formatDateNoT.js
@@ -0,0 +1,12 @@
+// 시간을 제외한 년월일이 보이는 경우에 사용하는 함수
+export const formatDateNoT = (dateStr) => {
+  const date = new Date(dateStr);
+
+  const year = date.getFullYear();
+  // +1을 제거해야 합니다. getMonth()에만 +1이 필요합니다
+  const month = String(date.getMonth() + 1).padStart(2, '0');
+  const day = String(date.getDate()).padStart(2, '0'); // +1 제거
+
+  const formatted = `${year}.${month}.${day}`;
+  return formatted;
+};
