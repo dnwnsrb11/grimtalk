4223a2f0da0f42f73d0c8c20f6200dda688d1235
WooJunGyu <dnwnsrb11@naver.com>
2025-02-05 17:27:15 +0900

    feat:이미지 추가

--- src/components/lecture/LectureBanner.jsx
+++ src/components/lecture/LectureBanner.jsx
@@ -1,13 +1,15 @@
-import BannerIMG from '@/assets/banner/lectureBannerIMG.png';
-
-export const LectureBanner = () => {
+export const LectureBanner = ({ lecture }) => {
   return (
     <>
       <div className="relative h-[300px] w-full overflow-hidden rounded-xl">
         <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-r from-white via-transparent to-transparent opacity-100"></div>
         <div className="absolute left-0 top-0 h-full w-full bg-gradient-to-l from-white via-transparent to-transparent opacity-100"></div>
         <div className="absolute bottom-0 left-0 h-full w-full bg-gradient-to-t from-white via-transparent to-transparent opacity-100"></div>
-        <img src={BannerIMG} alt="BannerIMG" className="h-full w-full rounded-xl object-cover" />
+        <img
+          src={lecture.image}
+          alt="BannerIMG"
+          className="h-full w-full rounded-xl object-cover"
+        />
       </div>
     </>
   );
