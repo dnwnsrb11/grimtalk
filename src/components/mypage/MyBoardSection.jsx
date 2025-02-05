14fda228688764c5acb3705512154e89c210c136
임재열 <dlawoduf15@naver.com>
2025-02-05 18:03:45 +0900

    feat: 내가 쓴 글 axios 제작

--- src/components/mypage/MyBoardSection.jsx
+++ src/components/mypage/MyBoardSection.jsx
@@ -1,10 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
+
+import { _axiosAuth } from '@/api/instance';
 import { CommunityList } from '@/components/mainPages/community/CommunityList';
 // 내가 쓴 글 컴포넌트
 export const MyBoardSection = () => {
   const boardList = Array.from({ length: 3 });
+  const { data: myQuestions } = useQuery({
+    queryKey: ['myQuestions'],
+    queryFn: async () => {
+      const { data } = await _axiosAuth.get('/mypage/board');
+
+      return data.body.data.list;
+    },
+  });
   return (
     <>
-      {boardList.map((_, index) => (
+      {myQuestions?.map((question, index) => (
         <CommunityList key={index} />
       ))}
     </>
