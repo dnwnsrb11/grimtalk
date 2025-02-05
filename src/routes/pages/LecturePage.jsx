39c8b517eb1f7b564108fa72c79eea722c518018
WooJunGyu <dnwnsrb11@naver.com>
2025-02-05 17:30:21 +0900

    feat:props 전달

--- src/routes/pages/LecturePage.jsx
+++ src/routes/pages/LecturePage.jsx
@@ -15,26 +15,6 @@ import { LectureReview } from '@/components/lecture/LectureReview';
 import { ReplayLecture } from '@/components/lecture/ReplayLecture';
 
 export const LecturePage = () => {
-  const [selectedCategory, setSelectedCategory] = useState('강의소개');
-  //   자식으로 부터 값을 받기 위한 함수
-  const handleCatagory = (childData) => {
-    setSelectedCategory(childData);
-  };
-  // 강사 여부 체크
-  const [checkInstructor, setCheckInstructor] = useState(true);
-
-  // 여기에 해당하는 컴포넌트를 저장한다.
-  // 자식 컴포넌트인 LectureCategory에서 값을 받아 catagory에 값을 넣어주면 값이 변경되어 아래에 다른 컴포넌트가 리랜더링 된다.
-  const MENU_COMPONENTS = {
-    강의소개: <IntroductionLecture />,
-    다시보기: <ReplayLecture checkInstructor={checkInstructor} />,
-    커리큘럼: <CurriculumLecture checkInstructor={checkInstructor} />,
-    default: <p>now Testing</p>,
-    공지사항: <LectureNotice checkInstructor={checkInstructor} />,
-    질문사항: <LectureQuestions checkInstructor={checkInstructor} />,
-    리뷰하기: <LectureReview />,
-  };
-
   // api 기능(강의 정보)
   const navigate = useNavigate();
   const { lectuerId } = useParams();
@@ -54,6 +34,26 @@ export const LecturePage = () => {
       navigate('/notfound');
     },
   });
+
+  const [selectedCategory, setSelectedCategory] = useState('강의소개');
+  //   자식으로 부터 값을 받기 위한 함수
+  const handleCatagory = (childData) => {
+    setSelectedCategory(childData);
+  };
+  // 강사 여부 체크
+  const [checkInstructor, setCheckInstructor] = useState(true);
+
+  // 여기에 해당하는 컴포넌트를 저장한다.
+  // 자식 컴포넌트인 LectureCategory에서 값을 받아 catagory에 값을 넣어주면 값이 변경되어 아래에 다른 컴포넌트가 리랜더링 된다.
+  const MENU_COMPONENTS = {
+    강의소개: <IntroductionLecture lecture={lecture} />,
+    다시보기: <ReplayLecture checkInstructor={checkInstructor} />,
+    커리큘럼: <CurriculumLecture checkInstructor={checkInstructor} />,
+    default: <p>now Testing</p>,
+    공지사항: <LectureNotice checkInstructor={checkInstructor} />,
+    질문사항: <LectureQuestions checkInstructor={checkInstructor} />,
+    리뷰하기: <LectureReview />,
+  };
 
   if (isLoading) {
     return <LoadingComponents />;
