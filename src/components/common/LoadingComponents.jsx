df2a172047bc1a943d64fe5ea57e6aa5e454f5ce
hoonixox <hoonixox@naver.com>
2025-02-06 10:03:41 +0900

    style: ESLint 코드 스타일 적용

--- src/components/common/LoadingComponents.jsx
+++ src/components/common/LoadingComponents.jsx
@@ -1,9 +1,9 @@
 export const LoadingComponents = () => {
   return (
     <>
-      <div className="flex items-center gap-3  w-full h-[calc(100vh-50px)] justify-center">
-        <span className="w-12 h-12 border-4 border-gray-border-color rounded-full inline-block relative box-border motion-safe:animate-[spin_1.5s_linear_infinite] after:content-[''] after:absolute after:left-0 after:top-0 after:bg-red-500 after:w-4 after:h-4 after:rounded-full after:transform after:-translate-x-1/2 after:translate-y-1/2"></span>
-        <h2 className="font-medium text-text-gray-color text-[20px]">
+      <div className="flex h-[calc(100vh-50px)] w-full  items-center justify-center gap-3">
+        <span className="relative box-border inline-block h-12 w-12 rounded-full border-4 border-gray-border-color after:absolute after:left-0 after:top-0 after:h-4 after:w-4 after:-translate-x-1/2 after:translate-y-1/2 after:transform after:rounded-full after:bg-red-500 after:content-[''] motion-safe:animate-[spin_1.5s_linear_infinite]"></span>
+        <h2 className="text-[20px] font-medium text-text-gray-color">
           현재 <span className="font-bold text-primary-color">로딩중</span> 입니다 . . .
         </h2>
       </div>
