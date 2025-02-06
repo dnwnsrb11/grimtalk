0cc0a5090128345e06c7c84ad0a59afb8cc65ce3
WooJunGyu <dnwnsrb11@naver.com>
2025-02-06 10:01:58 +0900

    rename: props 명칭 변경

--- src/components/lecture/CurriculumLecture.jsx
+++ src/components/lecture/CurriculumLecture.jsx
@@ -4,8 +4,8 @@ import { CurriculumLectureCardDeActive } from '@/components/lecture/curriculum/C
 export const CurriculumLecture = ({ checkInstructor, lecture }) => {
   // 받은 값이 ture, false에 따라 출력되는 컴포넌트가 다름
   //   index 추가
-  const selectListComponent = (isActive, index) => {
-    if (isActive === false) {
+  const selectListComponent = (checkInstructor, index) => {
+    if (checkInstructor === false) {
       return <CurriculumLectureCardDeActive key={index} />;
     } else {
       return <CurriculumLectureCardActive key={index} />;
@@ -17,7 +17,7 @@ export const CurriculumLecture = ({ checkInstructor, lecture }) => {
       <div className="mt-[60px]">
         <h1 className="text-[32px] font-bold">커리큘럼</h1>
         <div className="mt-[40px] flex flex-col gap-[30px]">
-          {test.map((isActive, index) => selectListComponent(isActive, index))}
+          {lecture.map((lecture, index) => selectListComponent(lecture, index))}
           {/* 전달 받은 값에 따라 다른 컴포넌트를 출력 */}
         </div>
       </div>
