43af5b23f2afffb5a828dac73cdb296d1bf87d10
WooJunGyu <dnwnsrb11@naver.com>
2025-02-07 12:54:07 +0900

    feat:작성 완료

--- src/components/lecture/question/QuestionLectureWrite.jsx
+++ src/components/lecture/question/QuestionLectureWrite.jsx
@@ -1,19 +1,83 @@
-export const QuestionLectureWrite = ({ setIsActive }) => {
+import { useMutation, useQuery } from '@tanstack/react-query';
+import { useState } from 'react';
+
+import { _axiosAuth } from '@/api/instance';
+import { LoadingComponents } from '@/components/common/LoadingComponents';
+
+export const QuestionLectureWrite = ({ setIsActive, curriculumId, lecture }) => {
+  const [subject, setSubject] = useState('');
+  const [content, setContent] = useState('');
+  // 커리큘럼 이름
+  const [curriculumName, setCurriculumName] = useState(null);
+  // 커리큘럼 조회
+  const {
+    data: curriculums,
+    isLoading,
+    isError,
+    error,
+  } = useQuery({
+    queryKey: ['curriculums'],
+    queryFn: async () => {
+      const { data } = await _axiosAuth.get(`/lecture-page/curriculum/${lecture.lectureId}`);
+      return data.body.data.list;
+    },
+    onError: () => {
+      navigate('/notfound');
+    },
+  });
+
+  // 질문 생성 api
+  const addQuestionMutaion = useMutation({
+    mutationFn: async () => {
+      const { data } = await _axiosAuth.post('/board', {
+        curriculumId: curriculumName,
+        subject: subject,
+        content: content,
+      });
+      return data;
+    },
+    onSuccess: () => {
+      setIsActive(false);
+      alert('질문이 작성되었습니다.');
+    },
+  });
   const changeActive = () => {
     setIsActive('/');
   };
+
+  if (isLoading) {
+    return <LoadingComponents />;
+  }
   return (
     <>
       <div>
+        <div className="mb-[10px] mt-[20px] flex gap-3 ">
+          <select
+            value={curriculumName}
+            onChange={(e) => setCurriculumName(e.target.value)}
+            className="h-10 w-full rounded-xl border border-gray-border-color p-2 pl-3 text-[#afafaf]"
+          >
+            <option value="">커리큘럼을 선택해주세요.</option>
+            {curriculums.map((curriculum, index) => (
+              <option value={curriculum.curriculumId} key={index}>
+                {curriculum.subject}
+              </option>
+            ))}
+          </select>
+        </div>
         <div className="flex flex-col gap-[15px]">
           <input
             type="text"
+            value={subject}
+            onChange={(e) => setSubject(e.target.value)}
             className="min-h-[60px] rounded-2xl border border-gray-border-color p-[20px] focus:border-primary-color focus:outline-none"
             placeholder="질문 제목을 입려해주세요."
           />
           <textarea
             className="min-h-[300px] resize-none rounded-2xl border border-gray-border-color p-[20px] focus:border-primary-color focus:outline-none"
             placeholder="질문 내용을 입력해주세요."
+            value={content}
+            onChange={(e) => setContent(e.target.value)}
           ></textarea>
         </div>
         <hr className="mt-[20px] border-gray-border-color" />
@@ -26,8 +90,11 @@ export const QuestionLectureWrite = ({ setIsActive }) => {
             >
               <p className="text-[18px] font-semibold">뒤로가기</p>
             </button>
-            <button className="rounded-2xl bg-primary-color px-[30px] py-[10px]">
-              <p className="text-[18px] font-semibold text-white">수정하기</p>
+            <button
+              className="rounded-2xl bg-primary-color px-[30px] py-[10px]"
+              onClick={() => addQuestionMutaion.mutate()}
+            >
+              <p className="text-[18px] font-semibold text-white">작성하기</p>
             </button>
           </div>
         </div>
