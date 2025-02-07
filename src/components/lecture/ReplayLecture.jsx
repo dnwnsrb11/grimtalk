23f743bf401deab61d686718d92e35346121c1a4
WooJunGyu <dnwnsrb11@naver.com>
2025-02-07 15:33:29 +0900

    feat:버튼 세분화를 위한 props 위치,내용 변경

--- src/components/lecture/ReplayLecture.jsx
+++ src/components/lecture/ReplayLecture.jsx
@@ -58,14 +58,13 @@ export const ReplayLecture = ({ checkInstructor, lecture }) => {
           <h1 className="text-[32px] font-bold">다시보기</h1>
           <div className="mt-[10px]">
             {replays.map((replay, index) => (
-              <div
-                key={index}
-                onClick={() => {
-                  setIsActive(true);
-                  setReplayDate(replay);
-                }}
-              >
-                <ReplayLectureCard replay={replay} checkInstructor={checkInstructor} />
+              <div key={index}>
+                <ReplayLectureCard
+                  replay={replay}
+                  checkInstructor={checkInstructor}
+                  setReplayDate={setReplayDate}
+                  setIsActive={setIsActive}
+                />
               </div>
             ))}
           </div>
