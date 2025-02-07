269f052ce2ca0a4501b24a9caaf62e55dbba5a6b
hoonixox <hoonixox@naver.com>
2025-02-07 16:50:57 +0900

    feat: Live 페이지 추가 및 라우팅

---
+++ src/routes/pages/LivePage.jsx
@@ -0,0 +1,9 @@
+import { Excalidraw } from '@excalidraw/excalidraw';
+
+export const LivePage = () => {
+  return (
+    <div className="h-screen w-full">
+      <Excalidraw className="h-full w-full" />
+    </div>
+  );
+};
