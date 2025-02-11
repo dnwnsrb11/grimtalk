23792bf42e35beda56638b78f34617b9f3372561
hoonixox <hoonixox@naver.com>
2025-02-11 21:59:21 +0900

    feat: livelist component에 live join 로직 추가

--- src/components/mainPages/home/LiveList.jsx
+++ src/components/mainPages/home/LiveList.jsx
@@ -1,15 +1,29 @@
 import { useNavigate } from 'react-router-dom';
 
 import testImg from '@/assets/banner/Test/TestImg.png';
+import { useLiveStore } from '@/store/useLiveStore';
+import { participantUtils } from '@/utils/participantUtils';
 
 // 라이브 방 카드 컴포넌트
 export const LiveList = ({ curriculumSubject, instructor }) => {
   const navigate = useNavigate();
 
+  const { setRoomCreator } = useLiveStore();
+
+  const handleJoinLive = async (selectedRoom, creator) => {
+    try {
+      localStorage.setItem('roomCreator', participantUtils.removeTokenPrefix(creator));
+      setRoomCreator(participantUtils.removeTokenPrefix(creator));
+      navigate(`/live/${selectedRoom}`);
+    } catch (error) {
+      alert('방 참여에 실패했습니다.');
+    }
+  };
+
   return (
     <div>
       <div className="relative min-h-[160px] overflow-hidden rounded-lg">
-        <button onClick={() => navigate(`/live/${curriculumSubject}`)}>
+        <button onClick={() => handleJoinLive(curriculumSubject, instructor)}>
           <div className="absolute left-[5%] top-[5%]">
             <div className="inline-block rounded-full bg-black bg-opacity-60 px-3 py-1">
               <div className="flex items-center gap-1">
