9a7d4d9dcfa141d3da295e90716cab43df6aef06
WooJunGyu <dnwnsrb11@naver.com>
2025-02-11 14:54:04 +0900

    desgin: 작업 카드 디자인 추가

--- src/components/replayPage/ReplayWorkList.jsx
+++ src/components/replayPage/ReplayWorkList.jsx
@@ -1,10 +1,11 @@
 export const ReplayWorkList = ({ data }) => {
   return (
     <>
-      <div className="flex items-center gap-2 rounded-2xl border px-[10px] py-[5px]">
+      <div className="flex items-center gap-2 rounded-2xl border border-gray-border-color px-[10px] py-[5px]">
+        <div className="h-2 w-2 rounded-full" style={{ background: data.element.strokeColor }} />
         <p>{data.element.strokeColor}</p>
-        <p>{data.time}</p>
-        <div className="h-1 w-1" style={{ background: data.element.strokeColor }} />
+        <span className="text-gray-border-color">|</span>
+        <p className="text-text-gray-color">{data.time / 10} 초</p>
       </div>
     </>
   );
