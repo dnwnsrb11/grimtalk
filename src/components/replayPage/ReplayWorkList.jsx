a3e68ee8cd3aba9206b835fef01e5692c5672423
WooJunGyu <dnwnsrb11@naver.com>
2025-02-11 13:46:34 +0900

    desing:작업 카드 디자인 수정

--- src/components/replayPage/ReplayWorkList.jsx
+++ src/components/replayPage/ReplayWorkList.jsx
@@ -1,7 +1,7 @@
 export const ReplayWorkList = ({ element }) => {
   return (
     <>
-      <div className="border p-10">
+      <div className="flex items-center gap-2 rounded-2xl border px-[10px] py-[5px]">
         <p>{element.strokeColor}</p>
         <div className="h-1 w-1" style={{ background: element.strokeColor }} />
       </div>
