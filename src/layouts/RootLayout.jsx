1876348929f13ff3baa86be9391b61f490ffc8b0
임재열 <dlawoduf15@naver.com>
2025-02-12 13:54:49 +0900

    design:navbar 수정

--- src/layouts/RootLayout.jsx
+++ src/layouts/RootLayout.jsx
@@ -28,7 +28,7 @@ export const RootLayout = () => {
           </div>
           <div className="col-span-2"></div>
         </div>
-        <footer>
+        <footer className="mt-[250px]">
           <Footer />
         </footer>
       </div>
