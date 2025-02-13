b266aff1d02963dab1128ea5f2aba7a3b2d8c618
WooJunGyu <dnwnsrb11@naver.com>
2025-02-13 17:28:27 +0900

    rename: 이름 변경

---
+++ src/components/ai/AiCompareComponent.jsx
@@ -0,0 +1,9 @@
+export const AiCompareComponent = () => {
+  return (
+    <>
+      <div>
+        <p>현재 로딩중...</p>
+      </div>
+    </>
+  );
+};
--- src/components/ai/AiComparePage.jsx
+++
@@ -1,7 +0,0 @@
-export const AiComparePage = () => {
-  return (
-    <>
-      <div></div>
-    </>
-  );
-};
