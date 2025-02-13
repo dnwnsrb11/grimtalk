ae4bb0410b2ee098f92a822722a54a2d94c9aaa1
WooJunGyu <dnwnsrb11@naver.com>
2025-02-13 14:46:36 +0900

    desgin: 워크 리스트 디자인 수정

--- src/components/replayPage/ReplayWorkList.jsx
+++ src/components/replayPage/ReplayWorkList.jsx
@@ -1,9 +1,9 @@
 export const ReplayWorkList = ({ data }) => {
   return (
     <>
-      <div className="flex items-center gap-2 rounded-2xl border border-gray-border-color bg-white/30 px-[10px] py-[5px] backdrop-blur-sm">
+      <div className="flex items-center gap-2 rounded-xl border border-gray-border-color bg-white/30 px-[10px] py-[5px] backdrop-blur-sm">
         <div className="h-2 w-2 rounded-full" style={{ background: data.element.strokeColor }} />
-        <p>{data.element.strokeColor}</p>
+        <p className="text-text-gray-color">{data.element.strokeColor}</p>
         <span className="text-gray-border-color">|</span>
         <p className="text-text-gray-color">{data.time / 10} 초</p>
       </div>
