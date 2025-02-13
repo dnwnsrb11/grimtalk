7c6e41418b0cb524257da34a2f6738fa0acc0c85
hoonixox <hoonixox@naver.com>
2025-02-13 20:17:40 +0900

    rename: lecture 오타 수정

--- src/routes/pages/LecturePage.jsx
+++ src/routes/pages/LecturePage.jsx
@@ -55,7 +55,7 @@ export const LecturePage = () => {
       // id가 undefined가 아닐 때만 체크
       setCheckInstructor(id === lecture.instructorInfo.id);
     }
-  }, [lectuerId, id, lecture]); // lecture나 id가 변경될 때마다 체크
+  }, [lectureId, id, lecture]); // lecture나 id가 변경될 때마다 체크
 
   const [selectedCategory, setSelectedCategory] = useState('강의소개');
   const contentRef = useRef(null);
