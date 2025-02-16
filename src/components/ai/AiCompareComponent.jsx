bfd660811b77811355d9543c313d906ad6a4f1d6
WJG <dnwnsrb11@naver.com>
2025-02-16 20:15:18 +0900

    feat:차트 컴포넌트 작업 생성

--- src/components/ai/AiCompareComponent.jsx
+++ src/components/ai/AiCompareComponent.jsx
@@ -1,9 +1,25 @@
-export const AiCompareComponent = () => {
+import { ResponsiveRadar } from '@nivo/radar';
+
+export const AiCompareComponent = ({ data }) => {
   return (
-    <>
-      <div>
-        <p>현재 로딩중...</p>
+    <div className="border">
+      <div style={{ width: '100%', height: '500px' }}>
+        <ResponsiveRadar
+          data={data}
+          keys={['myDrawing']} // 여기를 수정
+          indexBy="taste"
+          valueFormat=">-.2f"
+          margin={{ top: 80, right: 80, bottom: 80, left: 80 }}
+          borderColor="#FF5C38"
+          gridLabelOffset={36}
+          dotSize={15}
+          dotColor={{ theme: 'background' }}
+          dotBorderWidth={2}
+          colors="#ff0000"
+          blendMode="multiply"
+          motionConfig="wobbly"
+        />
       </div>
-    </>
+    </div>
   );
 };
