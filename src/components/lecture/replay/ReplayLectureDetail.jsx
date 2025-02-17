6cc3c37bf429fa4daf6ac4d9876d0cbab6a3c179
WooJunGyu <dnwnsrb11@naver.com>
2025-02-17 09:28:13 +0900

    feat:다시보기 버튼 활성화

--- src/components/lecture/replay/ReplayLectureDetail.jsx
+++ src/components/lecture/replay/ReplayLectureDetail.jsx
@@ -78,6 +78,10 @@ export const ReplayLectureDetail = ({ replayDate, setIsActive, checkInstructor }
     },
   });
 
+  const navigateReplay = () => {
+    navigate(`/replay/${replay.curriculumId}`);
+  };
+
   if (isLoading) {
     return <LoadingComponents />;
   }
@@ -89,23 +93,26 @@ export const ReplayLectureDetail = ({ replayDate, setIsActive, checkInstructor }
           <p className="text-[18px] font-medium">{replay.content}</p>
         </div>
         <div className="mt-[10px] flex items-center justify-center">
-          <button className="rounded-2xl border bg-primary-color px-[120px] py-[20px] text-center">
+          <button
+            className="bg-primary-color rounded-2xl border px-[120px] py-[20px] text-center"
+            onClick={navigateReplay}
+          >
             <p className="text-[18px] font-semibold text-white">다시보기</p>
           </button>
         </div>
       </div>
       {checkInstructor && (
         <div className="mt-[50px]">
-          <hr className="mt-[40px] border border-divider-color" />
+          <hr className="border-divider-color mt-[40px] border" />
           <div className="mt-[20px]">
             <h1 className="text-[32px] font-bold">다시보기 파일 업로드</h1>
             <div className="mt-[20px] flex justify-between gap-3">
               {video ? (
-                <div className="flex w-[85%] items-center rounded-2xl border bg-bg-gray-color">
+                <div className="bg-bg-gray-color flex w-[85%] items-center rounded-2xl border">
                   <p className="px-[20px] text-[18px] font-medium text-[#C6C6C6]">{video.name}</p>
                 </div>
               ) : (
-                <div className="flex w-[85%] items-center rounded-2xl border bg-bg-gray-color">
+                <div className="bg-bg-gray-color flex w-[85%] items-center rounded-2xl border">
                   <p className="px-[20px] text-[18px] font-medium text-[#C6C6C6]">
                     파일을 업로드 해주세요.
                   </p>
@@ -113,7 +120,7 @@ export const ReplayLectureDetail = ({ replayDate, setIsActive, checkInstructor }
               )}
               <label
                 htmlFor="upload-video"
-                className="w-[12%] cursor-pointer rounded-2xl border border-gray-border-color bg-bg-gray-color px-[25px] py-[20px] text-center text-[18px] font-semibold text-[#343434] transition-all duration-200 hover:bg-primary-color hover:text-white"
+                className="border-gray-border-color bg-bg-gray-color hover:bg-primary-color w-[12%] cursor-pointer rounded-2xl border px-[25px] py-[20px] text-center text-[18px] font-semibold text-[#343434] transition-all duration-200 hover:text-white"
               >
                 영상 찾기
               </label>
@@ -128,17 +135,17 @@ export const ReplayLectureDetail = ({ replayDate, setIsActive, checkInstructor }
           </div>
         </div>
       )}
-      <hr className="mt-[40px] border border-divider-color" />
+      <hr className="border-divider-color mt-[40px] border" />
       <div className="mt-[20px] flex justify-end gap-3">
         <button
-          className="rounded-2xl border border-gray-border-color bg-bg-gray-color p-[10px]"
+          className="border-gray-border-color bg-bg-gray-color rounded-2xl border p-[10px]"
           onClick={() => setIsActive(false)}
         >
           <p className="text-[18px] font-semibold">뒤로가기</p>
         </button>
         {checkInstructor && (
           <button
-            className="rounded-2xl border border-gray-border-color bg-primary-color p-[10px]"
+            className="border-gray-border-color bg-primary-color rounded-2xl border p-[10px]"
             onClick={() => addVideoMutaion.mutate()}
           >
             <p className="text-[18px] font-semibold text-white">자료 업로드</p>
