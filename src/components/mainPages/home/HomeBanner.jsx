630d023a39df31712f8662a965cf98b0c2e1c0c2
WooJunGyu <dnwnsrb11@naver.com>
2025-02-17 09:57:01 +0900

    feat:메인페이지에 배너 추가

---
+++ src/components/mainPages/home/HomeBanner.jsx
@@ -0,0 +1,17 @@
+export const HomeBanner = () => {
+  return (
+    <>
+      <div>
+        {/* 제목,소제목, 내용 */}
+        <div>
+          <h1 className="text-[64px] font-bold text-[#FF5C38]">강사의 붓 터치가 내 캠버스로 !</h1>
+          <h3>실시간 그림 공유로 더 가까운 배움</h3>
+          <p className="whitespace-pre-line">
+            강사의 손끝에서 완성되는 그림을 내 눈앞에서 그대로! 실시간으로 보고 따라 그리며,
+            자연스럽게 실력을 키워보세요.
+          </p>
+        </div>
+      </div>
+    </>
+  );
+};
