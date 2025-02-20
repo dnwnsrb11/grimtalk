ae86808e18f6f68bfd6a48f38ab931115a2a29da
임재열 <dlawoduf15@naver.com>
2025-02-20 19:40:55 +0900

    fix:구독자수에 따른 뱃지 멘트 관리

--- src/components/mypage/BadgeInformation.jsx
+++ src/components/mypage/BadgeInformation.jsx
@@ -11,6 +11,7 @@ export const BadgeInformation = ({
   badgeHeight,
   textSize,
 }) => {
+  console.log(calculateNextLevel(subscribeNumber).remainingSubscribers);
   return (
     <div className="flex h-full w-full flex-row items-center gap-5">
       <div className="flex min-h-[65px] min-w-[65px] items-center justify-center rounded-md bg-bg-gray-color p-3">
@@ -33,11 +34,17 @@ export const BadgeInformation = ({
           입니다.
         </p>
         <p className={`text-${textSize} text-[#6E6E6E]`}>
-          다음 뱃지까지 남은 구독자 수는{' '}
-          <span className="font-semibold text-black">
-            {calculateNextLevel(subscribeNumber).remainingSubscribers}명
-          </span>{' '}
-          입니다.
+          {calculateNextLevel(subscribeNumber).remainingSubscribers === 0 ? (
+            <span className="font-semibold text-black">최고 레벨에 도달하셨군요!</span>
+          ) : (
+            <>
+              다음 뱃지까지 남은 구독자 수는{' '}
+              <span className="font-semibold text-black">
+                {calculateNextLevel(subscribeNumber).remainingSubscribers}명
+              </span>{' '}
+              입니다.
+            </>
+          )}
         </p>
       </div>
     </div>
